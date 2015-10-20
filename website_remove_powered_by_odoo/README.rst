a54117f7313a8737ea61989f718fe129fe2ed677
rona <rona.lin@elico-corp.com>
2015-10-20 15:57:55 +0800

    add module website_remove_powered_by_odoo

---
+++ website_remove_powered_by_odoo/README.rst
@@ -0,0 +1,13 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==============================
+Website Remove Powered by Odoo
+==============================
+
+* This module allows you to remove Powered by Odoo from the website.
+* Note: This module interits the template from the webstie_sale. It may has conflict with certain themes.
+
+Please uninstall this module and update the website_sale if you failed
+to install the new theme.
---
+++ website_remove_powered_by_odoo/__openerp__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2010-2015 Elico Corp (<http://www.elico-corp.com>)
+#    Authors:  Rona Lin <rona.lin@elico-corp.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Website remove Powered by Odoo',
+    'version': '0.1',
+    'category': 'website',
+    'depends': ['website'],
+    'author': 'Elico Corp,Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'website': 'https://www.elico-corp.com',
+    'sequence': 1,
+    'images': [],
+    'demo': [],
+    'data': ['website_remove_powered_by_odoo.xml'],
+    'installable': True,
+    'application': False
+}
