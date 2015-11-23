34ee1fa90c836394fe6c7315a554e190c5f085f4
rona <rona.lin@elico-corp.com>
2015-11-23 17:06:04 +0800

    [IMP]10727 add module add_quota_in_report_menu.

---
+++ add_quota_in_report_menu/__openerp__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2010 Camptocamp SA (http://www.camptocamp.com)
+# All Right Reserved
+#
+# Author : xyz    (Elico Corp, xia.fajin@elico-corp.com)
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+{
+    "name": "Add quota menu in report",
+    "description": """
+    1@  Add quota menu in report for sale man
+    2@  Limit stock value on quant view to account group 
+
+========================================================
+                    """,
+    "version": "0.1",
+    "depends": [ "board","stock","base","account","hr"],
+    "category": "Reporting",
+    "author": "elico",
+    "url": "http://www.clico.com/",
+    "installable": True,
+    "auto_install": False,
+    "data" : ['add_quota_view.xml',],
+}
+
