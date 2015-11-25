e82dd4f38eedf8b8ce1d6476a13211b6029f1742
rona <rona.lin@elico-corp.com>
2015-11-25 18:04:59 +0800

    [IMP]add module project_type.

---
+++ project_type/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2015 Elico Corp (www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Project Configurable Type",
+    "summary": "Allow for Project specific type lists for Tasks",
+    "version": "9.0.1.0.0",
+    "category": "Project Management",
+    "website": "www.elico-corp.com",
+    "author": "Elico corp",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "project",
+    ],
+    "data": [
+        "project_type_view.xml",
+    ],
+}
