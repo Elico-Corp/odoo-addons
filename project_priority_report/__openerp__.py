b9e52160cdd2127d2463bf32ed66558ee9adf8ba
rona <rona.lin@elico-corp.com>
2015-11-29 16:52:17 +0800

    [FIX]fix RADME/openerp.py/PEP8.

--- project_priority_report/__openerp__.py
+++ project_priority_report/__openerp__.py
@@ -19,16 +19,22 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-{'name': 'Project Priority Report',
- 'version': '0.1',
- 'category': 'Human Resources',
- 'depends': ['hr_timesheet', 'project_timesheet', 'project_issue_sheet'],
- 'author': 'Elico Corp,Odoo Community Association (OCA)',
- 'license': 'AGPL-3',
- 'website': 'https://www.elico-corp.com',
- 'images': [],
- 'demo': [],
- 'data': ['report/project_priority_report_view.xml',
-          'security/ir.model.access.csv'],
- 'installable': True,
- 'application': False}
+{
+    'name': 'Project Priority Report',
+    'version': '8.0.1.0.0',
+    'category': 'Human Resources',
+    'depends': [
+        'hr_timesheet',
+        'project_timesheet',
+        'project_issue_sheet',
+    ],
+    'author': 'Elico Corp,Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'website': 'https://www.elico-corp.com',
+    'data': [
+        'report/project_priority_report_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': False,
+}
