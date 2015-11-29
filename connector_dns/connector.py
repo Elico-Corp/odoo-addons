529182c4c444ba54e247bd3c65c46f1f7f04e651
rona <rona.lin@elico-corp.com>
2015-11-29 14:08:59 +0800

    [FIX]sort import's.

--- connector_dns/connector.py
+++ connector_dns/connector.py
@@ -19,10 +19,10 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp import models, fields
+from openerp import fields, models
+from openerp.addons.connector.checkpoint import checkpoint
 from openerp.addons.connector.connector import (Environment,
                                                 install_in_connector)
-from openerp.addons.connector.checkpoint import checkpoint
 
 install_in_connector()
 
