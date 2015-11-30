050abe42a00a292ee16db3e57089aac82b7a8468
rona <rona.lin@elico-corp.com>
2015-11-30 12:08:34 +0800

    [IMP]use the simple header.

--- ldap_password/__init__.py
+++ ldap_password/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
-# © <2010-2014> <Augustin Cisterne-Kaas>
+# © <2010-2015> <Augustin Cisterne-Kaas>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from . import res_users
--- ldap_password/__openerp__.py
+++ ldap_password/__openerp__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-# © <2010-2014> <Augustin Cisterne-Kaas>
+# © 2015 Elico corp (www.elico-corp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     'name': 'Basic ldap user management through Odoo',
-    'version': '8.0.1.0.0',
+    'version': '8.0.1.0.1',
     'depends': [
         'auth_ldap'
     ],
@@ -11,9 +12,7 @@
     'license': 'AGPL-3',
     'website': 'https://www.elico-corp.com',
     'external_dependencies': {
-        'python': [
-            'ldap',
-        ],
+        'python': ['ldap'],
     },
     'installable': True,
     'application': False
