aecd4180a53f6169be716984c593e52ff90dde28
rona <rona.lin@elico-corp.com>
2015-12-01 15:37:56 +0800

    [FIX]fix the openerp.

--- auth_ldap_self_signed/__openerp__.py
+++ auth_ldap_self_signed/__openerp__.py
@@ -3,8 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Authentication via LDAP with self signed certificate',
-    'version': '0.1',
-    'category': '',
+    'version': '8.0.1.0.1',
     'depends': [
         'auth_ldap',
     ],
