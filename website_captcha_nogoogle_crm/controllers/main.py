313e77f2a81028d64d0870d587e5292c4f68e10d
rona <rona.lin@elico-corp.com>
2015-12-04 10:48:05 +0800

    [FIX]fix readme/pep8/header/isort.

--- website_captcha_nogoogle_crm/controllers/main.py
+++ website_captcha_nogoogle_crm/controllers/main.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# © 2015 Elico corp (www.elico-corp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openerp.addons.web import http
 from openerp.addons.web.http import request
 import openerp.addons.website_crm.controllers.main as main
