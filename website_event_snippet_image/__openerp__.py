f439aeaaf82fabc790f63b74bcf36391dff40118
rona <rona.lin@elico-corp.com>
2015-12-04 14:23:11 +0800

    [FIX]fix the readme/header.

--- website_event_snippet_image/__openerp__.py
+++ website_event_snippet_image/__openerp__.py
@@ -1,37 +1,19 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Odoo, Open Source Management Solution
-#    Copyright (c) 2010-2015 Elico Corp.
-#    Augustin Cisterne-Kaas <augustin.cisterne-kaas@elico-corp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-{'name': 'Website Event Snippet Image',
- 'version': '0.1',
- 'category': '',
- 'depends': ['website_event'],
- 'author': 'Elico Corp,Odoo Community Association (OCA)',
- 'license': 'AGPL-3',
- 'website': 'https://www.elico-corp.com',
- 'description': """
-
-""",
- 'images': [],
- 'demo': [],
- 'data': ['views/snippets.xml'],
- 'test': [],
- 'installable': True,
- 'application': False}
+# © 2015 Elico corp (www.elico-corp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Website Event Snippet Image',
+    'version': '8.0.1.0.1',
+    'category': 'website',
+    'depends': [
+        'website_event',
+    ],
+    'author': 'Elico Corp',
+    'license': 'AGPL-3',
+    'website': 'https://www.elico-corp.com',
+    'data': [
+        'views/snippets.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
