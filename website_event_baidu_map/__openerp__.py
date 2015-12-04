125064122a3583c72dfb121605243b0fd2e6df2a
rona <rona.lin@elico-corp.com>
2015-12-04 14:27:34 +0800

    [FIX]add Bug Tracker.

--- website_event_baidu_map/__openerp__.py
+++ website_event_baidu_map/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Website Event Baidu Map',
-    'version': '8.0.1.0.1',
+    'version': '8.0.1.0.2',
     'category': 'Website',
     'depends': [
         'website',
