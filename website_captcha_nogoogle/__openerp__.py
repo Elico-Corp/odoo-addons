b8d43d624e5d7f7f9122fb66b115ff76f372878b
rona <rona.lin@elico-corp.com>
2015-12-04 10:51:47 +0800

    [FIX]Remove price and currency.

--- website_captcha_nogoogle/__openerp__.py
+++ website_captcha_nogoogle/__openerp__.py
@@ -3,16 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Website Captcha',
-    'version': '8.0.1.0.0',
+    'version': '8.0.1.0.1',
     'category': 'Website',
     'depends': [
         'website',
     ],
     'author': 'Elico Corp',
-    # 'license': 'LGPL-3',
+    'license': 'LGPL-3',
     'website': 'https://www.elico-corp.com',
-    'currency': 'EUR',
-    'price': 19.9,
     'data': [
         'views/website_templates.xml',
         'views/res_config.xml',
