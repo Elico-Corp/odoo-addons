f9ff344dea04ba6b6025ee189840e1c161b4be10
rona <rona.lin@elico-corp.com>
2015-12-11 18:14:54 +0800

    [FIX]fix the readme/header/pep8.

--- sale_tracking_number/__openerp__.py
+++ sale_tracking_number/__openerp__.py
@@ -1,45 +1,19 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010-2015 Elico Corp (<http://www.elico-corp.com>)
-#    Authors: Siyuan Gu
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-{'name': 'Sale tracking number',
- 'version': '0.1',
- 'category': '',
- 'depends': ['sale'],
- 'author': 'Elico Corp',
- 'license': 'AGPL-3',
- 'website': 'https://www.elico-corp.com',
- 'description': """
-Sale tracking number
-=====================
-This model adds tracking number in sale order
-
-Contributors
-------------
-
-* Siyuan Gu: gu.siyuan@elico-corp.com
-
-""",
- 'images': [],
- 'demo': [],
- 'data': ['views/sale_view.xml'],
- 'test': [],
- 'installable': True,
- 'application': False}
+# © 2015 Elico corp (www.elico-corp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Sale tracking number',
+    'version': '8.0.1.0.1',
+    'category': 'sale',
+    'depends': [
+        'sale',
+    ],
+    'author': 'Elico Corp',
+    'license': 'AGPL-3',
+    'website': 'https://www.elico-corp.com',
+    'data': [
+        'views/sale_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
