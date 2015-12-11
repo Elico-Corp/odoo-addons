67fd8303f6b30e5310d688afb01eb2e9da114bb3
rona <rona.lin@elico-corp.com>
2015-12-11 17:57:34 +0800

    [FIX]fix readme/pep8/header.

--- auth_signup_error_message/controllers/main.py
+++ auth_signup_error_message/controllers/main.py
@@ -1,32 +1,16 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010-2015 Elico Corp (<http://www.elico-corp.com>)
-#    Authors: Siyuan Gu
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Elico corp (www.elico-corp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 import logging
+
 import werkzeug
 
+from openerp import http
+from openerp.addons.auth_signup.controllers.main import AuthSignupHome
 from openerp.addons.auth_signup.res_users import SignupError
 from openerp.http import request
-from openerp import http
 from openerp.tools.translate import _
-from openerp.addons.auth_signup.controllers.main import AuthSignupHome
 
 _logger = logging.getLogger(__name__)
 
@@ -58,7 +42,8 @@ class AuthSignupInherit(AuthSignupHome):
         """ Shared helper that creates a res.partner out of a token """
         values = dict((key, qcontext.get(key)) for key in (
             'login', 'name', 'password'))
-        assert any([k for k in values.values()]), "The form was not properly filled in."
+        assert any([k for k in values.values()]), "The form was not properly \
+        filled in."
         assert values.get('password') == qcontext.get(
             'confirm_password'), self.password_error_message
         self._signup_with_values(qcontext.get('token'), values)
