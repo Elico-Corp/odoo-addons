33cf6729bacf8a76ba428ce5ad2d70b773488d71
rona <rona.lin@elico-corp.com>
2015-12-14 16:32:14 +0800

    [FIX]fix the readme/isort/author/key.

--- web_login/__openerp__.py
+++ web_login/__openerp__.py
@@ -1,45 +1,16 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010-2015 Elico Corp (<http://www.elico-corp.com>)
-#    Authors: Siyuan Gu
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-{'name': 'Web Login',
- 'version': '0.1',
- 'category': '',
- 'depends': ['auth_signup'],
- 'author': 'Elico Corp',
- 'license': 'AGPL-3',
- 'website': 'https://www.elico-corp.com',
- 'description': """
-Website Login
-======================================================================
-This model redirect to the main of the website when log in.
-
-Contributors
-----------------------------------------------------------------------
-
-* Siyuan Gu: gu.siyuan@elico-corp.com
-
-""",
- 'images': [],
- 'demo': [],
- 'data': [],
- 'test': [],
- 'installable': True,
- 'application': False}
+# © 2015 Elico corp (www.elico-corp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Web Login',
+    'version': '8.0.1.0.1',
+    'category': 'website',
+    'depends': [
+        'auth_signup',
+    ],
+    'author': 'Elico Corp',
+    'license': 'AGPL-3',
+    'website': 'https://www.elico-corp.com',
+    'installable': True,
+    'application': False,
+}
