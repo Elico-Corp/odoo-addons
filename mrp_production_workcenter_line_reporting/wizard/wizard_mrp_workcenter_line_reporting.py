cc462995703ebd1ad28f92689bf4debf7387a373
rona <rona.lin@elico-corp.com>
2015-12-16 17:29:19 +0800

    [FIX] fix the key.y

--- mrp_production_workcenter_line_reporting/wizard/wizard_mrp_workcenter_line_reporting.py
+++ mrp_production_workcenter_line_reporting/wizard/wizard_mrp_workcenter_line_reporting.py
@@ -1,25 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010-2015 Elico Corp (<http://www.elico-corp.com>)
-#    Authors: Siyuan Gu
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp import models, fields, api
+# © 2015 Elico Corp (www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, fields, models
 
 
 class WizardMrpWorkcenterLineReporting(models.TransientModel):
@@ -49,7 +32,8 @@ class WizardMrpWorkcenterLineReporting(models.TransientModel):
             'date': self.date,
             'finished_qty': self.finished_qty,
             'scraped_qty': self.scraped_qty,
-            'scraped_reason_id': self.scraped_reason_id and self.scraped_reason_id.id or False
+            'scraped_reason_id':
+            self.scraped_reason_id and self.scraped_reason_id.id or False
         }
 
         obj.create(val)
