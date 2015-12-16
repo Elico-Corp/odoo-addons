3eac5edfa92e0cf0e99693545f226c2aeedb7b43
rona <rona.lin@elico-corp.com>
2015-12-16 15:38:05 +0800

    [FIX]fix the key/readme/pep8/isort.

--- excel_report_for_stock_valuation/__openerp__.py
+++ excel_report_for_stock_valuation/__openerp__.py
@@ -1,51 +1,21 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010-2015 Elico Corp (<http://www.elico-corp.com>)
-#    Authors: Siyuan Gu
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-{'name': 'Excel report for stock valutaion',
- 'version': '0.1',
- 'category': '',
- 'depends': ['stock_account', 'report_xls'],
- 'author': 'Elico Corp',
- 'license': 'AGPL-3',
- 'website': 'https://www.elico-corp.com',
- 'description': """
-Excel report for stock valuation
-===================================
-This module allows you to export excel report for stock
-Input/Output/Stock valuation during one specific period.
-
-Usage
------
-
-
-Contributors
-------------
-
-* Siyuan Gu: gu.siyuan@elico-corp.com
-""",
- 'images': [],
- 'demo': [],
- 'data': ['report/report.xml',
-          'wizard/report_stock_list.xml',
-          'wizard/report_stock.xml'],
- 'test': [],
- 'installable': True,
- 'application': False}
+# © 2015 Elico Corp (www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Excel report for stock valutaion',
+    'version': '8.0.1.0.1',
+    'depends': [
+        'stock_account',
+        'report_xls',
+    ],
+    'author': 'Elico Corp',
+    'license': 'AGPL-3',
+    'website': 'https://www.elico-corp.com',
+    'data': [
+        'report/report.xml',
+        'wizard/report_stock_list.xml',
+        'wizard/report_stock.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
