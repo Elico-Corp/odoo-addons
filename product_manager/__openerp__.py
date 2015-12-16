b24ab80c7b6e001995233cfa7d8dc10fc33b3de5
rona <rona.lin@elico-corp.com>
2015-12-16 15:12:19 +0800

    [FIX]fix the readme/isort/key.

--- product_manager/__openerp__.py
+++ product_manager/__openerp__.py
@@ -1,35 +1,18 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Author: Xia.Fajin
-#    Email: xia.fajin@elico-corp.com
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# -*- coding: utf-8 -*-
+# © 2015 Elico Corp (www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Manager",
-    "description": """
-
-    Build ACL for products.
-========================================================
-                    """,
-    "version": "0.1",
-    "depends": ["product", "purchase", "stock", "mrp", "base", ],
+    "version": "8.0.1.0.1",
+    "depends": [
+        "base",
+        "mrp",
+        "product",
+        "purchase",
+        "stock",
+    ],
     "category": "product",
-    "author": "elico-corp",
+    "author": "Elico Corp",
     "url": "http://www.elico-corp.com/",
     "data": [
         'data/res_group_data.xml',
@@ -38,5 +21,3 @@
     "installable": True,
     "auto_install": False,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
