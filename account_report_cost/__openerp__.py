c55e3af0776889fd4c985d96f1162f3c1e465b86
rona <rona.lin@elico-corp.com>
2015-12-17 18:00:14 +0800

    [FIX]fix the module's name.

--- account_report_cost/__openerp__.py
+++ account_report_cost/__openerp__.py
@@ -2,7 +2,7 @@
 # © 2015 Elico Corp (www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Account Reoprt Improvement',
+    'name': 'Account Reoprt Cost',
     'version': '8.0.1.0.1',
     'category': 'account',
     'depends': [
