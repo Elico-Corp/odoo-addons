6d5a390f24bf9b6bcf841b66132b03d7a393e1e4
rona <rona.lin@elico-corp.com>
2015-12-18 15:36:00 +0800

    [FIX]fix the readme/isort/pep8/key.

--- pos_printout_improvement/__openerp__.py
+++ pos_printout_improvement/__openerp__.py
@@ -1,53 +1,22 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010-2015 Elico Corp (<http://www.elico-corp.com>)
-#    Authors: Siyuan Gu
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-{'name': 'Pos printout improvement',
- 'version': '0.1',
- 'category': '',
- 'depends': ['pos_membership'],
- 'author': 'Elico Corp',
- 'license': 'AGPL-3',
- 'website': 'https://www.elico-corp.com',
- 'description': """
-Pos order improvement
-=====================
-This module adds customer information,
-order sequence and total quantity on the pos order printout.
-
-Usage
------
-* Several new rows display the customer information like: VIP code and name
-* A new column is created to display the sequence for each product.
-* A new line diplays the total quantity of the product.
-
-Contributors
-------------
-
-* Siyuan Gu: gu.siyuan@elico-corp.com
-
-""",
- 'images': [],
- 'demo': [],
- 'data': ['views/js.xml', ],
- 'qweb': ['static/src/xml/printout.xml', ],
- 'test': [],
- 'installable': True,
- 'application': False}
+# © 2015 Elico Corp (www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Pos printout improvement',
+    'version': '8.0.1.0.1',
+    'category': 'pos',
+    'depends': [
+        'pos_membership',
+    ],
+    'author': 'Elico Corp',
+    'license': 'AGPL-3',
+    'website': 'https://www.elico-corp.com',
+    'data': [
+        'views/js.xml',
+    ],
+    'qweb': [
+        'static/src/xml/printout.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
