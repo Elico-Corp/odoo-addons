2287a5aabb08f838f84541810ae1d99a738124ed
rona <ssauapw@qq.com>
2016-01-05 17:49:27 +0800

    [IMP]migrate baidumap form v8 to V9.

---
+++ website_baidu_map/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2016 Elico corp (www.elico-corp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Website Baidu Map',
+    'version': '9.0.1.0.1',
+    'category': 'map',
+    'depends': [
+        'website_event',
+        'base',
+    ],
+    'author': 'Elico Corp',
+    'license': 'AGPL-3',
+    'website': 'https://www.elico-corp.com',
+    'data': [
+        'website_baidu_map.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
