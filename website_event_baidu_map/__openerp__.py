fc2369c5106c8ab500855b1be50942630a1d5353
rona <ssauapw@qq.com>
2016-01-06 18:18:12 +0800

    [IMP]make the V9 possible to o have a property in odoo.

--- website_event_baidu_map/__openerp__.py
+++ website_event_baidu_map/__openerp__.py
@@ -2,18 +2,17 @@
 # © 2016 Elico corp (www.elico-corp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Website Baidu Map',
-    'version': '9.0.1.0.1',
-    'category': 'map',
+    'name': 'Website Event Baidu Map',
+    'version': '9.0.1.0.2',
+    'category': 'Website',
     'depends': [
         'website_event',
-        'base',
     ],
     'author': 'Elico Corp',
     'license': 'AGPL-3',
     'website': 'https://www.elico-corp.com',
     'data': [
-        'website_baidu_map.xml',
+        'website_event_baidu_map.xml',
     ],
     'installable': True,
     'application': False,
