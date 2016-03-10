1689e9e57f0101bf682a7e015f78e5cd2dd36d12
rona <ssauapw@qq.com>
2016-03-10 14:37:18 +0800

    [IMP]fix RADME/openerp.py/PEP8.

---
+++ add_quants_in_report_menu/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Elico Corp (www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Add Quota Menu In Report",
+    "version": "8.0.1.0.1",
+    "depends": [
+        "stock",
+    ],
+    "category": "Reporting",
+    "author": "Elico Corp",
+    "license": "AGPL-3",
+    "url": "www.elico-corp.com",
+    "application": False,
+    "installable": True,
+    "data": [
+        'add_quota_view.xml',
+    ],
+}
