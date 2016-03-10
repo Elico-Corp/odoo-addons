7b5980229feafcab68ce30fde000d745e5936b76
rona <ssauapw@qq.com>
2016-03-10 15:39:19 +0800

    [IMP]fix the readme and the module name.

--- stock_quant_report_menu/__openerp__.py
+++ stock_quant_report_menu/__openerp__.py
@@ -2,8 +2,8 @@
 # © 2016 Elico Corp (www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Add Quota Menu In Report",
-    "version": "8.0.1.0.1",
+    "name": "Stock Quant Report Menu",
+    "version": "8.0.1.0.2",
     "depends": [
         "stock",
     ],
@@ -14,6 +14,6 @@
     "application": False,
     "installable": True,
     "data": [
-        'add_quota_view.xml',
+        'stock_quant_report_view.xml',
     ],
 }
