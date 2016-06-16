a7aa2b9b213a9f7e1a0746c17ba7b2f177c479bc
Water <zzq_littleQ@163.name>
2016-06-16 15:29:45 +0800

    [IMP] migrate the  module from v7 to v8 language switch

--- sale_order_lines_discount/__openerp__.py
+++ sale_order_lines_discount/__openerp__.py
@@ -1,40 +1,19 @@
-# -*- encoding: utf-8 -*-
-# __author__ = xia.fajin@elico-corp.com
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2012 Cubic ERP - Teradata SAC (<http://cubicerp.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# -*- coding: utf-8 -*-
+# © 2016 Elico Corp (www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Order Lines Discount",
-    "description": """
-
-    Add function to change all  order lines discount once in sales order.
-========================================================
-                    """,
-    "version": "0.1",
-    "depends": ["sale"],
+    "version": "8.0.1.0.1",
     "category": "sale",
-    "author": "xia.fajin",
-    "url": "http://www.elico-corp.com/",
-    "data": ['sale_order_view.xml'],
+    "website": "www.elico-corp.com",
+    "author": "Elico Corp",
+    "license": "AGPL-3",
     "installable": True,
     "auto_install": False,
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "sale_order_view.xml",
+    ]
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- sale_order_lines_discount/sale_order_lines.py
+++ sale_order_lines_discount/sale_order_lines.py
@@ -1,46 +1,31 @@
-# -*- encoding: utf-8 -*-
-# __author__ = xia.fajin@elico-corp.com
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2012 Cubic ERP - Teradata SAC (<http://cubicerp.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2016 Elico Corp (www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
-from openerp.osv import fields, osv
+from openerp import fields, models, api
+from openerp.osv import osv
 from openerp.tools.translate import _
-from openerp.tools.safe_eval import safe_eval as eval
-import openerp.addons.decimal_precision as dp
-from openerp.tools.float_utils import float_round
-import math
 
-class sale_order(osv.osv):
+
+class SaleOrder(models.Model):
     _inherit = 'sale.order'
     _name = 'sale.order'
 
-    _columns = {
-        'all_discounts': fields.float('All order lines discount', required=True, help='Change all order lines discount.'),
-    }
+    all_discounts = fields.Float(
+        'All order lines discount',
+        required=True,
+        elp='Change all order lines discount.')
 
-    def update_discount_lines(self, cr, uid, ids, context=None):
-        if context.get('all_discounts', 0) < 100 and context.get('all_discounts', 0) >= 0:
-            lines = self.browse(cr, uid, ids, context=context).order_line
-            lines.write({'discount':context['all_discounts']})
-            return             
-        else:
-            raise osv.except_osv(_('Error!'),_("Your discount value is out of vlaues,it's value between 0 and 100,but 100 except."))
-
+    @api.multi
+    def update_discount_lines(self):
+        for sale_obj in self:
+            all_discounts = sale_obj.all_discounts
+            if all_discounts < 100 and all_discounts >= 0:
+                lines = sale_obj.order_line
+                lines.write({'discount': all_discounts})
+                return
+            else:
+                raise osv.except_osv(
+                    _('Error!'),
+                    _("""Your discount value is out of vlaues,
+                        it's value between 0 and 100,but 100 except."""))
