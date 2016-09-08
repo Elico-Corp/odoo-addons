d891a98d4e317148c2cc828164c87134cd049192
noah <234082230@qq.com>
2016-09-08 16:14:55 +0800

    [UPD] website_remove_powered_by_odoo

--- website_remove_powered_by_odoo/__openerp__.py
+++ website_remove_powered_by_odoo/__openerp__.py
@@ -5,16 +5,17 @@
     'name': 'Website remove Powered by Odoo',
     'version': '8.0.1.0.2',
     'category': 'website',
-    'depends': [
-        'website',
-    ],
+    'depends': ['website'],
     'author': 'Elico Corp',
     'license': 'AGPL-3',
+    'description': """
+        * This module allows you to remove Powered by Odoo from the website.
+        * Note: This module interits the template from the webstie_sale. It may has conflict with certain themes.
+        Please uninstall this module and update the website_sale if you failed to install the new theme.
+        """,
     'website': 'https://www.elico-corp.com',
     'sequence': 1,
-    'data': [
-        'website_remove_powered_by_odoo.xml',
-    ],
+    'data': ['website_remove_powered_by_odoo.xml'],
     'installable': True,
     'application': False
 }
