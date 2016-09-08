c93a904e425fd38be175510f2d589ecf9e3031ca
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2016-09-08 15:23:35 +0800

    [UPD] timesheet_activity_report

--- timesheet_activity_report/__openerp__.py
+++ timesheet_activity_report/__openerp__.py
@@ -1,22 +1,40 @@
 # -*- coding: utf-8 -*-
-# © 2015 Elico corp (www.elico-corp.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-{
-    'name': 'Timesheet Activities Report',
-    'version': '8.0.1.0.2',
-    'category': 'Human Resources',
-    'depends': [
-        'hr_timesheet',
-        'project_timesheet',
-        'project_issue_sheet',
-    ],
-    'author': 'Elico Corp',
-    'license': 'AGPL-3',
-    'website': 'https://www.elico-corp.com',
-    'data': [
-        'report/timesheet_activity_report_view.xml',
-        'security/ir.model.access.csv',
-    ],
-    'installable': True,
-    'application': False
-}
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2010-2014 Elico Corp (<http://www.elico-corp.com>)
+#    Authors: Sebastien Maillard
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{'name': 'Timesheet Activities Report',
+ 'version': '8.0.1.2.0',
+ 'category': 'Human Resources',
+ 'depends': [
+     'project_timesheet',
+     'project_issue_sheet',
+     'project_task_category',
+     'project_project_category',
+     'business_requirement'
+ ],
+ 'author': 'Elico Corp',
+ 'license': 'AGPL-3',
+ 'website': 'https://www.elico-corp.com',
+ 'images': [],
+ 'demo': [],
+ 'data': ['report/timesheet_activity_report_view.xml',
+          'security/ir.model.access.csv'],
+ 'installable': True,
+ 'application': False}
