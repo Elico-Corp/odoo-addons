99936568179827b63c32f5ef9856b61f823b6b23
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2016-09-08 14:53:14 +0800

    [UPD] auth_ldap_self_signed from superman

--- auth_ldap_self_signed/__openerp__.py
+++ auth_ldap_self_signed/__openerp__.py
@@ -1,14 +1,37 @@
 # -*- coding: utf-8 -*-
-# © 2015 Elico corp (www.elico-corp.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-{
-    'name': 'Authentication via LDAP with self signed certificate',
-    'version': '8.0.1.0.1',
-    'depends': [
-        'auth_ldap',
-    ],
-    'author': 'Elico Corp,Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False
-}
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2010-2014 Elico Corp (<http://www.elico-corp.com>)
+#    Authors: Augustin Cisterne-Kaas
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{'name': 'Authentication via LDAP with self signed certificate',
+ 'version': '0.1',
+ 'category': '',
+ 'depends': ['auth_ldap'],
+ 'author': 'Elico Corp',
+ 'license': 'AGPL-3',
+ 'website': 'https://www.elico-corp.com',
+ 'description': """
+Allows self signed certificate when using LDAP with TLS
+""",
+ 'images': [],
+ 'demo': [],
+ 'data': [],
+ 'installable': True,
+ 'application': False}
+
