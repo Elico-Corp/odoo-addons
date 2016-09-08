4b4b0580b4593088e23b205ffcae14c7db9feb89
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2016-09-08 16:46:07 +0800

    [UPD] update website_certificate from superman

--- website_certificate/__openerp__.py
+++ website_certificate/__openerp__.py
@@ -1,20 +1,33 @@
 # -*- coding: utf-8 -*-
-# © 2016 Elico Corp (www.elico-corp.com).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-{
-    'name': 'Website Certificate',
-    'version': '8.0.1.0.4',
-    'category': 'website',
-    'depends': [
-        'website',
-        'base',
-    ],
-    'author': 'Elico Corp',
-    'license': 'AGPL-3',
-    'website': 'https://www.elico-corp.com',
-    'data': [
-        'certificate_view.xml',
-    ],
-    'installable': True,
-    'application': False
-}
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2010-2015 Elico Corp (<http://www.elico-corp.com>)
+#    Authors: Liu Lixia
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{'name': 'Website Certificate',
+ 'version': '8.0.0.0.3',
+ 'category': 'website',
+ 'depends': ['base', 'website'],
+ 'author': 'Elico-Corp,Odoo Community Association (OCA)',
+ 'license': 'AGPL-3',
+ 'website': 'https://www.elico-corp.com',
+ 'images': [],
+ 'demo': [],
+ 'data': ['certificate_view.xml'],
+ 'installable': True,
+ 'application': False}
