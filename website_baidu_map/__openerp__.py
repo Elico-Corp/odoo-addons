26944e4562d4d666edfa8eef7c5926a2185f77a4
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2016-09-08 18:15:21 +0800

    [UPD] update py file header

--- website_baidu_map/__openerp__.py
+++ website_baidu_map/__openerp__.py
@@ -1,34 +1,17 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010-2015 Elico Corp (<http://www.elico-corp.com>)
-#    Authors: Liu Lixia
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-{'name': 'Website Baidu Map',
- 'version': '0.1',
- 'category': 'map',
- 'depends': ['website', 'base'],
- 'author': 'Elico Corp',
- 'license': 'AGPL-3',
- 'website': 'https://www.elico-corp.com',
- 'images': [],
- 'demo': [],
- 'data': ['website_baidu_map.xml',
-          ],
- 'installable': True,
- 'application': False}
+#   2016 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Website Baidu Map',
+    'version': '0.1',
+    'category': 'map',
+    'depends': ['website', 'base'],
+    'author': 'Elico Corp',
+    'license': 'AGPL-3',
+    'website': 'https://www.elico-corp.com',
+    'images': [],
+    'demo': [],
+    'data': ['website_baidu_map.xml'],
+    'installable': True,
+    'application': False
+}
