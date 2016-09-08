8913ad9e111d69f80fd6189e85a5a4aab5156f4c
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2016-09-08 15:15:57 +0800

    [ADD] task_id_display

---
+++ task_id_display/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2010-2014 Elico Corp. All Rights Reserved.
+#    Bob Lucas <luo.yumeng@elico-corp.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Task ID Display',
+    'version': '0.1',
+    'category': '',
+    'depends': ['project', 'project_issue'],
+    'author': 'Elico Corp',
+    'license': 'AGPL-3',
+    'website': 'https://www.elico-corp.com',
+    'description': """
+    Add several fields to the CRM form
+    """,
+    'images': [],
+    'demo': [],
+    'data': [
+        'project_task_view.xml',
+        'project_issue_view.xml'
+    ],
+    'installable': True,
+    'applications': False,
+}
