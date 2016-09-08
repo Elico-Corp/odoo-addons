d4cba8b44c1c961066d7b92a7a87edfcc667115b
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2016-09-08 16:42:26 +0800

    [ADD] website_baidu_map_multi

---
+++ website_baidu_map_multi/models/company_shop.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from openerp import fields, models
+
+
+class CompanyShop(models.Model):
+    _name = 'company.shop'
+
+    name = fields.Char(string='Name', required=True)
+    city = fields.Char(string='City', required=True)
+    address = fields.Char(string='Address', required=True)
+    company_id = fields.Many2one(comodel_name='res.company',
+                                 string='Company')
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    shop_ids = fields.One2many(comodel_name='company.shop',
+                               inverse_name='company_id',
+                               string='shop')
