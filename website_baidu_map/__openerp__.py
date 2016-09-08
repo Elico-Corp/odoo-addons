329ade03aca81cd3c41b2a55d3b94a9f4d4fc5ee
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2016-09-08 18:25:09 +0800

    [UPD] update version

--- website_baidu_map/__openerp__.py
+++ website_baidu_map/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Website Baidu Map',
-    'version': '0.1',
+    'version': '8.0.1.0.1',
     'category': 'map',
     'depends': ['website', 'base'],
     'author': 'Elico Corp',
