a9c615e7b49d42664fd6f7799950ea7dd3140ae4
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2016-09-09 10:43:15 +0800

    [UPD] update py file header

--- timesheet_activity_report/__openerp__.py
+++ timesheet_activity_report/__openerp__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# © 2015 Elico corp (www.elico-corp.com)
+#   2016 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Timesheet Activities Report',
-    'version': '8.0.1.0.2',
+    'version': '8.0.1.2.0',
     'category': 'Human Resources',
     'depends': [
          'project_timesheet',
