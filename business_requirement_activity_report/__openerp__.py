964072d81125879bf89dc6ad68c2172cb9d261a3
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2016-09-12 11:26:30 +0800

    [UPD] rename timesheet_activity_report -> business_requirement_activity_report

--- business_requirement_activity_report/__openerp__.py
+++ business_requirement_activity_report/__openerp__.py
@@ -2,7 +2,7 @@
 #   2016 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Timesheet Activities Report',
+    'name': 'Business Requirement Activity Report',
     'version': '8.0.1.2.0',
     'category': 'Human Resources',
     'depends': [
