86878e5c33c1d52ba0835a2618e14c3eeed065b4
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2016-09-12 10:48:46 +0800

    [UPD] for code review

---
+++ account_prepayment/README.rst
@@ -0,0 +1,30 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==================
+Account Prepayment
+==================
+
+Fine tune Prepayment addon \n
+* Add Prepayment Account In Partner \n
+* Add purchase_order_id and Prepayment Account in payment.
+
+
+Contributors
+------------
+
+* Andy Lu <andy.lu@elico-corp.com>
+
+Maintainer
+----------
+
+.. image:: https://www.elico-corp.com/logo.png
+:alt: Elico Corp
+:target: https://www.elico-corp.com
+
+This module is maintained by Elico Corporation.
+
+Elico Corporation offers consulting services to implement open source management software in SMEs, with a strong involvement in quality of service.
+
+Our headquarters are located in Shanghai with branches in Hong Kong, ShenZhen and Singapore servicing customers from Greater China, Asia Pacific, Europe, Americas, etc...
--- account_prepayment/models/account.py
+++ account_prepayment/models/account.py
@@ -1,54 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010-2013 Elico Corp. All Rights Reserved.
-#    Author: Andy Lu <andy.lu@elico-corp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2015 Elico corp (www.elico-corp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openerp.osv import osv, fields
 from openerp.tools.translate import _
-
-
-class res_partner(osv.osv):
-    _inherit = 'res.partner'
-
-    # _columns = {
-    #     'property_account_prepayable': fields.property(
-    #         'account.account',
-    #         type='many2one',
-    #         relation='account.account',
-    #         string="Account payable (Prepayment)",
-    #         view_load=True,
-    #         domain="[('type', '=', 'payable')]",
-    #         help="This account will be used instead of the default one as the \
-    #         Prepayment payable account for the current partner",
-    #         required=True),
-    #     'property_account_prereceivable': fields.property(
-    #         'account.account',
-    #         type='many2one',
-    #         relation='account.account',
-    #         string="Account Receivable (Prepayment)",
-    #         view_load=True,
-    #         domain="[('type', '=', 'receivable')]",
-    #         help="This account will be used instead of the default one as the \
-    #         Prepayment receivable account for the current partner",
-    #         required=True),
-    # }
 
 
 class account_voucher(osv.osv):
@@ -56,7 +10,8 @@ class account_voucher(osv.osv):
 
     _columns = {
         'purchase_id': fields.many2one(
-            'purchase.order', 'Purchase Order',
+            'purchase.order',
+            'Purchase Order',
             domain=[('invoiced', '=', False)],
             ondelete='set null'),
         'use_prepayment_account': fields.boolean(
@@ -64,12 +19,12 @@ class account_voucher(osv.osv):
             help="Check this if you want to input a prepayment \
             on the prepayment accounts."),
         'sale_id': fields.many2one(
-            'sale.order', 'Sale Order', domain=[('invoiced', '=', False)],
+            'sale.order',
+            'Sale Order',
+            domain=[('invoiced', '=', False)],
             ondelete='set null'),
     }
-    _defaults = {
-        'use_prepayment_account': False,
-    }
+    _defaults = {'use_prepayment_account': False, }
 
     def onchange_sale_id(self, cr, uid, ids, sale_id):
         res = {}
@@ -83,8 +38,7 @@ class account_voucher(osv.osv):
                               'message': _('Selected Sale Order was paid.')}
         for invoice in so.invoice_ids:
             amount = invoice.residual
-        res['value'] = {'partner_id': so.partner_id.id,
-                        'amount': amount}
+        res['value'] = {'partner_id': so.partner_id.id, 'amount': amount}
         return res
 
     def onchange_purchase_id(self, cr, uid, ids, purchase_id):
@@ -101,20 +55,11 @@ class account_voucher(osv.osv):
 
         for invoice in po.invoice_ids:
             amount = invoice.residual
-        res['value'] = {'partner_id': po.partner_id.id,
-                        'amount':  amount}
+        res['value'] = {'partner_id': po.partner_id.id, 'amount': amount}
         return res
 
-    # def onchange_prepayment_account(self, cr, uid, ids,
-    #                                 use_prepayment_account):
-    #     res = {}
-    #     if not use_prepayment_account:
-    #         return res
-
-    #     res['value'] = {'line_cr_ids': [], 'line_dr_ids': [], 'line_ids': []}
-    #     return res
-    def onchange_prepayment_account(self, cr, uid, ids,
-                                    use_prepayment_account):
+    def onchange_prepayment_account(
+            self, cr, uid, ids, use_prepayment_account):
         res = {}
         line_cr_id = ""
         if not use_prepayment_account:
@@ -125,16 +70,30 @@ class account_voucher(osv.osv):
         res['value'] = {'line_cr_ids': [], 'line_dr_ids': [], 'line_ids': []}
         return res
 
-    def writeoff_move_line_get(self, cr, uid, voucher_id, line_total, move_id,
-                               name, company_currency, current_currency,
+    def writeoff_move_line_get(self,
+                               cr,
+                               uid,
+                               voucher_id,
+                               line_total,
+                               move_id,
+                               name,
+                               company_currency,
+                               current_currency,
                                context=None):
         line_vals = super(account_voucher, self).writeoff_move_line_get(
-            cr, uid, voucher_id, line_total, move_id, name, company_currency,
-            current_currency, context=context)
+            cr,
+            uid,
+            voucher_id,
+            line_total,
+            move_id,
+            name,
+            company_currency,
+            current_currency,
+            context=context)
         if line_vals:
             account_id = False
-            voucher_brw = self.pool.get(
-                'account.voucher').browse(cr, uid, voucher_id, context)
+            voucher_brw = self.pool.get('account.voucher').browse(
+                cr, uid, voucher_id, context)
             if voucher_brw.use_prepayment_account:
                 if voucher_brw.payment_option == 'with_writeoff':
                     account_id = voucher_brw.writeoff_acc_id.id
@@ -158,7 +117,8 @@ class account_voucher(osv.osv):
                 if account_id:
                     line_vals['account_id'] = account_id
         return line_vals
-account_voucher()
 
+
+account_voucher()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
