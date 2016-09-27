3fbcf0a2fd9794c0cce79021565b935cc2079a33
noah <234082230@qq.com>
2016-09-27 17:31:50 +0800

    [ADD] elico_crm_extra

---
+++ elico_crm_extra/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2010-2013 Elico Corp. All Rights Reserved.
+#    Author: Jon chow <jon.chow@elico-corp.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Elico CRM',
+    'version': '1.0',
+    'author': 'Elico Corp',
+    'website': 'http://www.elico-corp.com',
+    'summary': '',
+    'description' : """
+         access rule: case 
+    """,
+    'depends': ['base', 'crm'],
+    'category': '',
+    'sequence': 10,
+    'demo': [],
+    'data': [
+        'crm_view.xml',
+        'security/ir.model.access.csv',
+        ],
+    'test': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'css': [],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
