9536ef5ee6b2312139680d5dea8d6c0013cd1732
noah <234082230@qq.com>
2016-11-01 18:29:26 +0800

    move project_partner_name from project_partner_mandatory

---
+++ project_partner_name/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Elico Corp (www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Project Partner Name",
+    "version": "8.0.1.0.0",
+    'category': 'Project Management',
+    "website": "www.elico-corp.com",
+    "author": "Elico corp",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "project",
+    ],
+    "data": [
+        "views/project_view.xml",
+    ],
+}
