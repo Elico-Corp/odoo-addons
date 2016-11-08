391c02a0bf918a1bef39b668c238a1499947e927
franksongfeng <43217183@qq.com>
2016-11-08 14:45:51 +0800

    make _get_partner_ref() as member function of the class AnalyticAccount again

--- project_partner_name/models/analytic_account_id.py
+++ project_partner_name/models/analytic_account_id.py
@@ -8,10 +8,18 @@ from openerp import api, models
 class AnalyticAccount(models.Model):
     _inherit = 'account.analytic.account'
 
+    def _get_partner_ref(self, proj):
+        if proj.partner_id:
+            if proj.partner_id.is_company and proj.partner_id.ref:
+                return proj.partner_id.ref
+            elif proj.partner_id.parent_id and proj.partner_id.parent_id.ref:
+                return proj.partner_id.parent_id.ref
+        return False
+
     def _get_full_names(self, elmt, level):
 
+        # Tail recursion function
         def iter_parent_ids(elmt, level, full_names):
-
             if level <= 0:
                 full_names.append('...')
                 return (None, 0, full_names)
@@ -56,9 +64,11 @@ class AnalyticAccount(models.Model):
                         context=context
                     )
 
-                    partner_ref = project_obj._get_partner_ref()[0]
+                    partner_ref = self._get_partner_ref(project_obj)
 
-                    full_names[0] = "%s - %s" % (partner_ref, full_names[0])
+                    if partner_ref:
+                        full_names[0] = "%s - %s" % (partner_ref, full_names[0])
+
 
                 full_names.reverse()  # order on First Root Last Leaf
                 
