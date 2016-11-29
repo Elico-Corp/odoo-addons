c37b5026edbab24fbbc1ee7256637d9f58721e6d
Matjaz Mozetic <sysadmin@matmoz.si>
2016-11-29 07:12:42 +0100

    Correct typo in init file

--- elico_mail_batch_read/wizard/__init__.py
+++ elico_mail_batch_read/wizard/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # © 2015 Elico corp (www.elico-corp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import read_multi_mails
+from . import read_multiple_mails
