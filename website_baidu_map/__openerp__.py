795f55f440b927e3ae92417bee26164491762545
Noah Wang - Elico <234082230@qq.com>
2016-12-19 15:12:11 +0800

    Revert "[IMP]change the api to the latest v2.0."

---
+++ website_baidu_map/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#   2016 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Website Baidu Map',
+    'version': '8.0.1.0.1',
+    'category': 'map',
+    'depends': ['website', 'base'],
+    'author': 'Elico Corp',
+    'license': 'AGPL-3',
+    'website': 'https://www.elico-corp.com',
+    'images': [],
+    'demo': [],
+    'data': ['website_baidu_map.xml'],
+    'installable': True,
+    'application': False
+}
--- website_event_baidu_map/__openerp__.py
+++ website_event_baidu_map/__openerp__.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-#   2016 Elico Corp (https://www.elico-corp.com).
+# © 2015 Elico corp (www.elico-corp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Website Event Baidu Map',
-    'version': '8.0.1.0.3',
+    'version': '8.0.1.0.2',
     'category': 'Website',
     'depends': [
+        'website',
+        'base',
         'website_event',
     ],
     'author': 'Elico Corp',
@@ -15,5 +17,5 @@
         'website_event_baidu_map.xml',
     ],
     'installable': True,
-    'application': False
+    'application': False,
 }
