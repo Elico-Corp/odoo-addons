715e9f07d830894ea908e29fd36bc447d367d046
franksongfeng <frank.song@elico-corp.com>
2017-01-11 11:02:27 +0800

    add modification to version in __openerp__.py

--- membership_account_balance/__openerp__.py
+++ membership_account_balance/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Membership Management - Member Account Balance',
-    'version': '0.1',
+    'version': '8.0.1.0.1',
     'category': '',
     'depends': ['account', 'membership', 'base_setup'],
     'author': 'Elico Corp',
