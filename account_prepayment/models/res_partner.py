c3a43f8d8fda88639a0c8c76b7be465ead31c034
noah <234082230@qq.com>
2017-02-07 14:48:57 +0800

    format

--- account_prepayment/models/res_partner.py
+++ account_prepayment/models/res_partner.py
@@ -12,13 +12,15 @@ class res_partner(osv.osv):
             'account.account',
             string="Account Payable (Prepayment)",
             domain="[('type', '=', 'payable')]",
-            help="This account will be used instead of the default one as the prepayment payable account for the current partner",
+            help="This account will be used instead of the default one as the \
+                prepayment payable account for the current partner",
             required=True),
         'property_account_prereceivable': fields.many2one(
             'account.account',
             string="Account Receivable (Prepayment)",
             domain="[('type', '=', 'receivable')]",
-            help="This account will be used instead of the default one as the prepayment receivable account for the current partner",
+            help="This account will be used instead of the default one as the \
+                prepayment receivable account for the current partner",
             required=True),
     }
 
