904ad394db1ce123fe235e20e45d7082e00f76cf
noah <234082230@qq.com>
2017-02-14 16:07:47 +0800

    [ADD] snippet_google_map

---
+++ snippet_google_map/__openerp__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Google Map Snippet',
+    'category': 'Website',
+    'version': '1.1',
+    'author': 'Odoo S.A.,Odoo Community Association (OCA)',
+    'data': [
+        'views/assets.xml',
+        'views/s_google_map.xml',
+    ],
+    'depends': ['theme_common'],
+}
