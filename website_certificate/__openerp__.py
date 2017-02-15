8f9c6db45cd0f15a41a82aece38d96a01bdbb72d
noah <234082230@qq.com>
2017-02-15 11:01:32 +0800

    [FIX] travis

--- website_certificate/__openerp__.py
+++ website_certificate/__openerp__.py
@@ -9,7 +9,6 @@
     'author': 'Elico-Corp,Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'website': 'https://www.elico-corp.com',
-    'support': '',
     'images': [],
     'demo': [],
     'data': ['certificate_view.xml'],
