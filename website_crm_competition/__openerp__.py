f9277328841f42776aa92a0441cd9a4dda43725f
noah <234082230@qq.com>
2017-03-07 15:47:56 +0800

    travis

--- website_crm_competition/__openerp__.py
+++ website_crm_competition/__openerp__.py
@@ -2,18 +2,13 @@
 # © 2016 Elico corp (www.elico-corp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {'name': 'Website contact competition',
- 'version': '0.1',
+ 'version': '8.0.1.0.0',
  'category': '',
  'depends': ['website_crm'],
  'author': 'Elico Corp',
  'license': 'AGPL-3',
  'website': 'https://www.elico-corp.com',
- 'description': """
-
-""",
- 'images': [],
- 'demo': [],
+ 'support': 'support@elico-corp.com',
  'data': ['views/website_crm.xml'],
- 'test': [],
  'installable': True,
  'application': False}
