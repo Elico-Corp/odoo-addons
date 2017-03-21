15cc3c3c3b4ee15d578382e1dd0c36e1af968ef4
Victor Martin <victor.martin@elico-corp.com>
2017-03-21 20:14:28 +0800

    [WIP] business_requirement_deliverable_project_task_mandatory

---
+++ business_requirement_deliverable_project_task_mandatory/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Business Requirement Deliverable Project Task Mandatory",
+    "version": "8.0.1.0.0",
+    "website": "https://odoo-community.org/",
+    "author": "",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "project_project_category",
+        "business_requirement_deliverable_project",
+    ],
+    "data": [
+        "views/project_project_category_view.xml",
+    ],
+}
---
+++ business_requirement_deliverable_project_task_mandatory/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import project_project_category
+from . import project_task
