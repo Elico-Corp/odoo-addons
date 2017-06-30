910c9bf1ec90397f406f75cb708cd476e2f668ec
Shukai Dong <dongshuki@163.com>
2017-06-30 14:20:41 +0800

    Refactor the reminder email
    Signed-off-by: Shukai Dong <dongshuki@163.com>

--- timesheet_automatic_reminder/models/__init__.py
+++ timesheet_automatic_reminder/models/__init__.py
@@ -3,5 +3,3 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import common_method
-from . import timesheet_employee_reminder
-from . import timesheet_manager_reminder
--- timesheet_automatic_reminder/models/common_method.py
+++ timesheet_automatic_reminder/models/common_method.py
@@ -2,62 +2,338 @@
 # © 2015-2017 Elico corp (http://www.elico-corp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import time
-from datetime import datetime, timedelta, date
-import pytz
+from datetime import datetime, timedelta
+from collections import defaultdict
 
-# from openerp import models, api
-
-DATETIME = '%Y-%m-%d %H:%M:%S'
-DATE = '%Y-%m-%d'
-TIME = '%H:%M:%S'
+from openerp import models, api, fields
+from openerp.tools import DEFAULT_SERVER_TIME_FORMAT as TIME_FORMAT, \
+    DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT, \
+    DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT
 
 
-def is_weekend(selected_date):
-    """
-    The function is used judge the weekend date,
-    if is weekend return True else return False
-    :param selected_date: the date need judged date,
-    just support string date format '%Y-%m-%d %H:%M:%S' or datetime type
-    :return: True or False
-    """
-    if not isinstance(selected_date, (datetime, date)):
-        selected_date = datetime.strptime(selected_date, DATETIME)
+class RemindDateLine(models.TransientModel):
+    _name = 'reminder.date.line'
+    _description = 'Date need reminder lines'
 
-    return True if selected_date.weekday() in [5, 6] else False
-
-
-def str_to_datetime(date_str, tz_info=None, new_tz=None):
-    """
-    Translation string date into datetime date
-    :param date_str:
-    :param tz_info:
-    :param new_tz:
-    :return:
-    """
-    if new_tz and isinstance(new_tz, str):
-        new_tz = pytz.timezone(new_tz)
-
-    res = datetime.strptime(
-        time.strftime(date_str),
-        DATETIME
-    )
-    if tz_info:
-        res = res.replace(tzinfo=tz_info)
-    if new_tz:
-        res = res.replace(
-            tzinfo=pytz.utc
-        ).astimezone(new_tz)
-
-    return res
+    # columns
+    reminder_date = fields.Date('Reminder Date')
+    leave_hours = fields.Float('Leave Hours')
+    work_hours = fields.Float('Work Hours')
+    lack_hours = fields.Float('Lack Hours')
+    reminder_id = fields.Many2one('timesheet.reminder', 'Reminder Timesheet',
+                                  ondelete='cascade')
 
 
-def tz_offset_today(employee_tz_str):
-    """
-    Get Today's Date format data
-    :param employee_tz_str: The employee's timezone
-    :return:
-    """
-    employee_tz = pytz.timezone(employee_tz_str)
-    utc_date = datetime.today().replace(tzinfo=pytz.utc)
-    return utc_date.astimezone(employee_tz).date()
+class TimesheetReminderCommon(models.TransientModel):
+    _name = 'timesheet.reminder'
+    _description = 'TimeSheet Reminder'
+    _inherit = ['mail.thread']
+
+    # columns
+    employee_id = fields.Many2one('hr.employee', 'Employee')
+    manager_id = fields.Many2one('hr.employee', 'Employee Manager',
+                                 related='employee_id.parent_id', store=True)
+    reminder_date_ids = fields.One2many('reminder.date.line', 'reminder_id',
+                                        string='Reminder Date')
+
+    @staticmethod
+    def _get_leave_to_days(leave, work_start, work_end, employee_leave_dict):
+        """
+        Distinguish the vacation days to separate dates
+        :param leave: need distinguished leave record
+        :param period_date: need sorted days
+        :param employee_leave_dict: result
+        :return: employee_leave_dict
+        """
+        employee = leave.employee_id
+        _start_leave = fields.Datetime.from_string(leave.date_from)
+        start_leave = fields.Datetime.context_timestamp(employee, _start_leave)
+        _end_leave = fields.Datetime.from_string(leave.date_to)
+        end_leave = fields.Datetime.context_timestamp(employee, _end_leave)
+        diff_days = (end_leave - start_leave).days
+        count = 0
+        check_date = start_leave.date()
+        start_leave_date = start_leave.date()
+        end_leave_date = end_leave.date()
+        one_day = timedelta(days=1)
+        while count <= diff_days:
+            if check_date.strftime(DATE_FORMAT) in employee_leave_dict[employee.id]:
+                if start_leave_date == check_date and end_leave_date == check_date:
+                    _sl, _el = start_leave, end_leave
+                elif start_leave_date == check_date and end_leave_date != check_date:
+                    _sl = start_leave
+                    _el = datetime.combine(check_date,
+                                           datetime.strptime('23:59:59', TIME_FORMAT).time())\
+                        .replace(tzinfo=start_leave.tzinfo)
+                elif start_leave_date != check_date and end_leave_date == check_date:
+                    _el = end_leave
+                    _sl = datetime.combine(check_date,
+                                           datetime.strptime('00:00:00', TIME_FORMAT).time())\
+                        .replace(tzinfo=end_leave.tzinfo)
+                else:
+                    employee_leave_dict[employee.id][check_date.strftime(DATE_FORMAT)] += 9
+                    count += 1
+                    check_date += one_day
+                    continue
+                work_on = datetime.combine(check_date, work_start).replace(tzinfo=_sl.tzinfo)
+                work_off = datetime.combine(check_date, work_end).replace(tzinfo=_el.tzinfo)
+                leave_on = _sl if _sl > work_on else work_on
+                leave_off = _el if _el < work_off else work_off
+                leave_hours = leave_off.hour - leave_on.hour
+                employee_leave_dict[employee.id][check_date.strftime(DATE_FORMAT)] += \
+                    leave_hours if leave_hours > 0 else 0
+            count += 1
+            check_date += one_day
+        return employee_leave_dict
+
+    @api.model
+    def _get_employee_leaves(self, yesterday, count_days, work_start,
+                             work_end, employee_records=None):
+        """
+        Get the leave time about every employee
+        :param start_datetime:
+        :param end_datetime:
+        :param employee_records:
+        :return:
+        """
+        end_datetime = yesterday.strftime(DATETIME_FORMAT)
+        count_date = [yesterday.date()]
+        while len(count_date) < count_days:
+            s = (count_date[-1] - timedelta(days=1))
+            count_date.append(s)
+        start_datetime = count_date[-1].strftime(DATETIME_FORMAT)
+        holiday_obj = self.env['hr.holidays']
+        if not employee_records:
+            employee_records = self.env['hr.employee'].search([])
+        count_date_str = [r.strftime(DATE_FORMAT) for r in count_date]
+        # employee_leave_dict = dict.\
+        #     fromkeys(employee_records.ids,
+        #              dict.fromkeys(deepcopy(count_date_str), 0))
+        employee_leave_dict = defaultdict(dict)
+        for employee in employee_records:
+            employee_leave_dict[employee.id] = dict.fromkeys(count_date_str, 0)
+        leave_records = holiday_obj.search([
+            ('employee_id', 'in', employee_records.ids),
+            ('type', '=', 'remove'),
+            ('state', 'not in', ['cancel', 'refuse']),
+            '|', '&',
+            ('date_from', '>=', start_datetime),
+            ('date_from', '<=', end_datetime),
+            '&',
+            ('date_to', '>=', start_datetime),
+            ('date_to', '<=', end_datetime)
+        ], order='date_from')
+        for leave in leave_records:
+            employee_leave_dict = self._get_leave_to_days(leave, work_start,
+                                                          work_end, employee_leave_dict)
+        return employee_leave_dict
+
+    @api.model
+    def _get_employee_tms(self, end_date, count_days, employee_records=False):
+        """
+
+        :param start_date:
+        :param count_days:
+        :param work_start:
+        :param work_end:
+        :param employee_records:
+        :return:
+        """
+        end_date_str = end_date.strftime(DATE_FORMAT)
+        start_date = end_date - timedelta(days=count_days)
+        start_date_str = start_date.strftime(DATE_FORMAT)
+        if not employee_records:
+            employee_records = self.env['hr.employee'].search([])
+
+        user_employee_dict = defaultdict(set)
+        [user_employee_dict[x.user_id.id].add(x.id) for x in employee_records]
+
+        tms_obj = self.env['hr.analytic.timesheet']
+        tms_records = tms_obj.search([
+            ('user_id', 'in', user_employee_dict.keys()),
+            ('date', '>=', start_date_str),
+            ('date', '<=', end_date_str)
+        ])
+        employee_timesheet_dict = defaultdict(dict)
+        for tms in tms_records:
+            user_id = tms.user_id.id
+            tms_date = tms.date
+            for emp_id in user_employee_dict.get(user_id, []):
+                if tms.date not in employee_timesheet_dict[emp_id]:
+                    employee_timesheet_dict[emp_id][tms_date] = 0
+                employee_timesheet_dict[emp_id][tms_date] += tms.unit_amount
+
+        return employee_timesheet_dict
+
+    @api.model
+    def _start_remind(self, count_days, workstart, workend,
+                      rest_hours, reminder_type=1):
+        """
+        The cron job start part, find the employee who didn't input his TMS and create the
+        record for reminder his manager in the noon
+        :param count_days: how many days need check
+        :param workstart: the time when you start work(work on)
+        :param workend: the time when you end work(work off)
+        :param resethours: during the work time how many hours you get to reset
+        :param reminder_type: Email to employee(1) or manager(2)
+        :return:
+        """
+        delta_one_day = timedelta(days=1)
+        employee_records = self.env['hr.employee'].search([])
+        _yesterday = (datetime.now() - delta_one_day)
+        workstart_time = datetime.strptime(workstart, TIME_FORMAT).time()
+        workend_time = datetime.strptime(workend, TIME_FORMAT).time()
+        employee_leave_dict = self._get_employee_leaves(_yesterday, count_days,
+                                                        workstart_time, workend_time,
+                                                        employee_records=employee_records)
+        employee_tms_dict = self._get_employee_tms(_yesterday, count_days,
+                                                   employee_records=employee_records)
+        need_work_hours = workend_time.hour - workstart_time.hour - rest_hours
+        reminder_records = self.\
+            _create_employee_reminder_records(_yesterday, count_days, employee_records,
+                                              employee_leave_dict, employee_tms_dict,
+                                              need_work_hours)
+        if reminder_type == 2:
+            self._send_email_reminder_manager(reminder_records=reminder_records)
+        else:
+            self._send_email_reminder_employee(reminder_records=reminder_records)
+
+        return True
+
+    @api.model
+    def _check_is_working_day(self, check_date, employee_id=None):
+        """
+        Check the date belong to working day or rest day
+        :param selected_date: the date need to checked
+        :param employee_id: the employee need checked
+        :return: if the check_date is working day return True, otherwise return False
+        """
+        public_holiday_obj = self.env['hr.holidays.public']
+        if isinstance(check_date, basestring):
+            check_date = fields.Date.from_string(check_date)
+        _is_weekend = True if check_date.weekday() in [5, 6] else False
+        _is_public_holiday = public_holiday_obj.\
+            is_public_holiday(check_date, employee_id=employee_id)
+
+        return not (_is_weekend or _is_public_holiday)
+
+    @api.model
+    def _create_employee_reminder_records(self, end_date, count_days, employee_records,
+                                          employee_leave_dict, employee_tms_dict,
+                                          need_work_hours):
+        """
+
+        :param end_date:
+        :param count_days:
+        :param employee_records:
+        :param employee_leave_dict:
+        :param employee_tms_dict:
+        :param need_work_hours:
+        :return:
+        """
+        result = self.env['timesheet.reminder']
+        self.search([('create_date', '=', fields.Date.today())]).unlink()
+        check_date_list = [end_date - timedelta(x) for x in xrange(count_days)]
+        for employee in employee_records:
+            emp_id = employee.id
+            real_check_list = filter(lambda x: self._check_is_working_day(x, emp_id),
+                                     check_date_list)
+            reminder_date_ids = []
+            for check_date in real_check_list:
+                check_date_str = check_date.strftime(DATE_FORMAT)
+                leave_hours = employee_leave_dict.get(emp_id, {}).get(check_date_str, 0)
+                tms_hours = employee_tms_dict.get(emp_id, {}).get(check_date_str, 0)
+                effective_hours = leave_hours + tms_hours
+
+                if need_work_hours > effective_hours:
+                    lack_hours = need_work_hours - effective_hours
+                    reminder_date_ids.append([0, False, {
+                        'leave_hours': leave_hours,
+                        'work_hours': tms_hours,
+                        'lack_hours': lack_hours,
+                        'reminder_date': check_date_str
+                    }])
+            result |= self.create({
+                'employee_id': emp_id,
+                # 'manager_id', employee.parent_id.id,
+                'reminder_date_ids': reminder_date_ids,
+            })
+        return result
+
+    @api.model
+    def _send_email_reminder_employee(self, reminder_records, force=False):
+        """
+
+        :param reminder_records:
+        :param force:
+        :return:
+        """
+        email_from = self.env.user.company_id.email
+        data = self.env['ir.model.data']
+        mail = self.env['mail.mail']
+        template_xmlid = 'employee_reminder_email_template'
+        # if reminder_type == 2:
+        #     template_xmlid = 'manager_reminder_email_template'
+        template = data.xmlid_to_object(
+            'timesheet_automatic_reminder.{}'.format(template_xmlid))
+        mail_ids = []
+        for reminder in reminder_records:
+            emp_work_email = reminder.employee_id.work_email
+            if emp_work_email and email_from and (email_from != emp_work_email or force):
+                mail_id = template.with_context(dbname=self.env.cr.dbname).send_mail(reminder.id)
+                the_mailmess = mail.browse(mail_id).mail_message_id
+                the_mailmess.write({'model': None, 'res_id': False})
+                mail_ids.append(mail_id)
+
+        if mail_ids:
+            res = mail.send(mail_ids)
+
+        return res
+
+
+    @api.model
+    def _send_email_reminder_manager(self, reminder_records, force=False):
+        """
+
+        :param reminder_records:
+        :param force:
+        :return:
+        """
+        email_from = self.env.user.company_id.email
+        data = self.env['ir.model.data']
+        mail = self.env['mail.mail']
+        template_xmlid = 'manager_reminder_email_template'
+        template = data.xmlid_to_object(
+            'timesheet_automatic_reminder.{}'.format(template_xmlid))
+        mail_ids = []
+        # Get the manger and employee mappping
+        manager_employee_dict = {}
+        for reminder in reminder_records:
+            manager = reminder.employee_id.parent_id
+            if not manager:
+                continue
+            key = manager   # (manager.id, manager.name)
+            if key not in manager_employee_dict:
+                manager_employee_dict[key] = {
+                    'manager': manager,
+                    'employee': {},
+                    'res_id': reminder.id
+                }
+            reminder_date = (', ').join([x.reminder_date for x in reminder.reminder_date_ids])
+            emp_key = (reminder.employee_id.id, reminder.employee_id.name)
+            manager_employee_dict[key]['employee'][emp_key] = reminder_date
+
+        for manager_record, real_content in manager_employee_dict.items():
+            emp_work_email = manager_record.work_email
+            res_id = real_content.pop('res_id')
+            if emp_work_email and email_from and (emp_work_email != email_from or force):
+                mail_id = template.with_context(manager=real_content).\
+                    send_mail(res_id)
+                the_mailmess = mail.browse(mail_id).mail_message_id
+                the_mailmess.write({'model': None, 'res_id': False})
+                mail_ids.append(mail_id)
+
+        if mail_ids:
+            res = mail.send(mail_ids)
+
+        return res
