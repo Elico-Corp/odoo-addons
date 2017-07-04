ea2396e456b1c2608cabb4b648448cb680b409ff
Shukai Dong <dongshuki@163.com>
2017-07-04 17:24:29 +0800

    modify the travis error
    Signed-off-by: Shukai Dong <dongshuki@163.com>

--- project_task_wbs/project.py
+++ project_task_wbs/project.py
@@ -2,7 +2,7 @@
 # © 2015-2017 Elico corp (http://www.elico-corp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields,models
+from openerp import fields, models
 
 
 class ProjectTask(models.Model):
