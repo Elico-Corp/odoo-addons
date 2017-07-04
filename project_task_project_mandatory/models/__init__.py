00dc6326f2a6bc7e9b5e4c61f3d8aa7571823440
Shukai Dong <dongshuki@163.com>
2017-07-04 17:50:40 +0800

    Submit the PR for project_task_project_mandatory
    Signed-off-by: Shukai Dong <dongshuki@163.com>

---
+++ project_task_project_mandatory/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2015-2017 Elico corp (http://www.elico-corp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import project
+from . import task
