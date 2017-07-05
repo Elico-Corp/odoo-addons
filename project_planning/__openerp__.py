8c70cb0493ab4d3a6028b92c1db6c178357ccd05
Shukai Dong <dongshuki@163.com>
2017-07-05 16:07:45 +0800

    Modify the code after recheck the module elico-project
    Signed-off-by: Shukai Dong <dongshuki@163.com>

--- project_planning/__openerp__.py
+++ project_planning/__openerp__.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-# © 2015-2017 Elico corp (http://www.elico-corp.com)
+# © 2015-2017 Elico corp (https://www.elico-corp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Project Planning',
     'version': '8.0.1.0.0',
     'category': 'Project',
     'depends': [
-        'project_task_type',
-        'project_task_wbs',
+        'project_task_category',
     ],
     'author': 'Elico Corp',
     'license': 'AGPL-3',
@@ -15,7 +14,7 @@
     'data': [
         'project_task_view.xml',
         'project_tasks_planning_view.xml',
-        'reset_planning_wizard_view.xml',
+        'wizard/reset_planning_wizard_view.xml',
     ],
     'installable': True,
     'application': False,
