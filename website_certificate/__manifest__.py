f3b8289ea9b193d563c8fa06e46870ce08488bfe
Eric @ Elico Corp <eric.caudal@elico-corp.com>
2017-08-15 08:52:36 +0800

    Update and rename __openerp__.py to __manifest__.py

--- website_certificate/__manifest__.py
+++ website_certificate/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Website Certificate',
-    'version': 10.0.1.0.1',
+    'version': '10.0.1.0.1',
     'category': 'website',
     'depends': [
         'website',
