2d89d696725dec3db74f21d4e2e7492b6006cd44
Murtuza Saleh <m.saleh.serpentcs@serpentcs.com>
2017-08-29 15:26:08 +0530

    [WIP][MIG][10.0-Issue175-T23295]: Migrate project_project_category module.

--- project_project_category/__manifest__.py
+++ project_project_category/__manifest__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-# © 2016 Elico Corp (www.elico-corp.com).
+# © 2016-2017 Elico Corp (https://www.elico-corp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     "name": "Project Configurable Category",
     "version": "10.0.1.0.0",
@@ -9,14 +10,14 @@
     'support': 'support@elico-corp.com',
     "author": "Elico corp",
     "license": "AGPL-3",
-    "application": False,
-    "installable": False,
     "depends": [
         "project",
     ],
     "data": [
+        'data/project_project_category_data.xml',
         'security/ir.model.access.csv',
         'views/project_project_view.xml',
         'views/project_project_category_view.xml',
     ],
+    "installable": True,
 }
