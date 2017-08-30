7b54b8a6545144c1336fc86e0eeae0a491a7d9d7
Sébastien Maillard <sebastien.maillard@elico-corp.com>
2017-08-31 04:40:16 +0800

    Breaking version upgrade

--- project_completion_report/__openerp__.py
+++ project_completion_report/__openerp__.py
@@ -4,7 +4,7 @@
 {
     "name": "Project Completion Report",
     "summary": "Follow-up project completion (estimated / consumed)",
-    "version": "8.0.1.0.2",
+    "version": "8.0.2.0.0",
     "category": "Project",
     "website": "https://odoo-community.org/",
     "author": "Sebastien Maillard, Elico Corp",
