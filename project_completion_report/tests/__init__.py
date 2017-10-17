7005d1ddedbba3897660fffef2ba78674f478b87
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2017-10-17 17:55:46 +0530

    [IMP]Improved code

--- project_completion_report/tests/__init__.py
+++ project_completion_report/tests/__init__.py
@@ -1,2 +1,5 @@
-import test_sale_timesheet
-import test_sale_service
+# -*- coding: utf-8 -*-
+# © 2016 Elico Corp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import test_report
--- project_completion_report/tests/test_report.py
+++ project_completion_report/tests/test_report.py
@@ -1,45 +1,56 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo.addons.sale.tests.test_sale_common import TestSale
+# © 2016 Elico Corp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo.tests import common
 
 
-class TestSaleService(TestSale):
+class TestSaleService(common.TransactionCase):
+    def setUp(self):
+        super(TestSaleService, self).setUp()
 
-    def test_sale_service(self):
-        """ Test task creation when confirming a so with the corresponding product """
+    def test_report(self):
         project_id = self.env.ref('project.project_project_1')
-        # let's log some timesheets
-        self.env['account.analytic.line'].create({
-            'name': 'Test Line',
-            'project_id': project_id.id,
-            'unit_amount': 10.5,
-        })
-
-        self.issue = self.env['project.issue'].create({
-            'name': 'Issue name',
-            'project_id': self.project.id,
-        })
-        analytic_account = self.env['account.analytic.account'].create({
-            'name': 'test account',
-        })
-
         self.ProjectObj = self.env['project.project']
 
         self.AnalyticAccountObject = self.env['account.analytic.account']
-
+        self.AnalyticLinAccountObject = self.env['account.analytic.line']
         self.AnalyticAccount = self.AnalyticAccountObject.create(
             {'name': 'AnalyticAccount for Test'})
+
+        # Configure unit of measure.
+        self.categ_wtime = self.ref('product.uom_categ_wtime')
+        self.categ_kgm = self.ref('product.product_uom_categ_kgm')
 
         self.projectA = self.ProjectObj.\
             create({'name': 'Test Project A', 'partner_id': 1,
                     'analytic_account_id': self.AnalyticAccount.id})
-
+        self.projectB = self.ProjectObj.\
+            create({'name': 'Test Project B', 'partner_id': 1,
+                    'analytic_account_id': self.AnalyticAccount.id})
+        self.UomObj = self.env['product.uom']
+        self.uom_hours = self.UomObj.create({
+            'name': 'Test-Hours',
+            'category_id': self.categ_wtime,
+            'factor': 8,
+            'uom_type': 'smaller'})
+        self.uom_days = self.UomObj.create({
+            'name': 'Test-Days',
+            'category_id': self.categ_wtime,
+            'factor': 1})
+        self.uom_kg = self.UomObj.create({
+            'name': 'Test-KG',
+            'category_id': self.categ_kgm,
+            'factor_inv': 1,
+            'factor': 1,
+            'uom_type': 'reference',
+            'rounding': 0.000001})
 
         self.ProductObj = self.env['product.product']
         self.productA = self.ProductObj.create(
             {'name': 'Product A', 'uom_id': self.uom_hours.id,
-                'uom_po_id': self.uom_hours.id,
-                'standard_price': 450})
+            'uom_po_id': self.uom_hours.id,
+            'standard_price': 450
+             })
         self.productB = self.ProductObj.\
             create({'name': 'Product B', 'uom_id': self.uom_hours.id,
                     'uom_po_id': self.uom_hours.id,
@@ -53,34 +64,38 @@ class TestSaleService(TestSale):
                     'uom_po_id': self.uom_kg.id,
                     'standard_price': 750})
 
-
-        self.task_1 = self.env['project.task'].with_context({
-            'mail_create_nolog': True}).create({
-            'name': 'Pigs UserTask',
-            'project_id': project_id.id
-        })
-
-        self.task_2 = self.env['project.task'].with_context({
-            'mail_create_nolog': True}).create({
-            'name': 'Pigs ManagerTask',
-            'user_id': self.user_projectmanager.id,
-            'project_id': self.project_pigs.id
-        })
-
-
-
         vals = {
             'description': 'test',
-            'project_id': self.projectA.id,
-            'task_ids': [(0, 0, {'name': 'Test Task'}),
-                         (0, 0, {'name': 'Test Task2'})],
+            'project_id': project_id.id,
+            'task_ids': [
+                (0, 0, {
+                    'name': 'Test Line',
+                    'project_id': project_id.id,
+                    'planned_hours': 10,
+                }),
+                (0, 0, {
+                    'name': 'Test Line',
+                    'project_id': project_id.id,
+                    'planned_hours': 30,
+                })]
         }
+        ts1 = self.AnalyticLinAccountObject.create({
+            'name': 'Test Line',
+            'project_id': project_id.id,
+            'unit_amount': 30,
+        })
 
-        br_obj = self.env['business.requirement']
-        br_obj = br_obj.new({'project_id': self.projectA.id})
-        br_obj.project_id_change()
+        ts2 = self.AnalyticLinAccountObject.create({
+            'name': 'Test Line',
+            'project_id': project_id.id,
+            'unit_amount': 40,
+        })
 
-        vals.update({'partner_id': br_obj.partner_id.id})
+        ts3 = self.AnalyticLinAccountObject.create({
+            'name': 'Test Line',
+            'project_id': project_id.id,
+            'unit_amount': 25,
+        })
 
         self.brA = self.env['business.requirement'].create(vals)
         self.brA.write({
@@ -116,13 +131,16 @@ class TestSaleService(TestSale):
                         'unit_price': 1500, 'uom_id': 1,
                         })
             ]})
-
-        self.env['account.analytic.line'].create({
-            'name': 'Test Line',
-            'project_id': project_id.id,
-            'unit_amount': 39.5,
-            'user_id': self.user.id,
-        })
-        self.assertEqual(so.order_line.qty_delivered, 50, 'Sale Timesheet: timesheet does not increase delivered quantity on so line')
-        self.assertEqual(so.invoice_status, 'invoiced', 'Sale Timesheet: "invoice on order" timesheets should not modify the invoice_status of the so')
+        planned_hours = 0
+        remaining_hours = 0
+        for hours in self.brA.task_ids:
+            planned_hours += hours.planned_hours
+        remaining_hours += ts1.unit_amount + ts2.unit_amount + ts3.unit_amount
+        rl_hours = 0
+        for rl in self.brA.deliverable_lines:
+            for hours in rl.resource_ids:
+                rl_hours += hours.qty
+        extra_hours = rl_hours - remaining_hours
+        self.assertEqual(extra_hours, 105.0)
+        self.assertEqual(planned_hours, 40.0)
 
