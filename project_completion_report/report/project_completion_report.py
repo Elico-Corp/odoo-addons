9b0ef8b744dc5d123cee83b0b8985c4da10bf238
Sébastien Maillard <sebastien.maillard@elico-corp.com>
2017-10-27 17:56:16 +0800

    Rename `extra_hours` to `deviation`

--- project_completion_report/report/project_completion_report.py
+++ project_completion_report/report/project_completion_report.py
@@ -62,7 +62,7 @@ class ProjectCompletionReport(models.Model):
         'Remain. time', digits=(16, 2), readonly=True,
         help="Remaining time")
     total_hours = fields.Float('Total time', digits=(16, 2), readonly=True)
-    extra_hours = fields.Float('Extra time', digits=(16, 2), readonly=True)
+    deviation = fields.Float('Deviation', digits=(16, 2), readonly=True)
 
     def init(self, cr):
         """Project Completion Report"""
@@ -97,7 +97,7 @@ class ProjectCompletionReport(models.Model):
                                 + t.remaining_hours AS total_hours,
                             COALESCE(SUM(al.unit_amount), 0)
                                 + t.remaining_hours - COALESCE(r.qty, 0)
-                                AS extra_hours
+                                AS deviation
                         FROM
                             project_project p
                             -- Link with the analytic account
@@ -141,7 +141,7 @@ class ProjectCompletionReport(models.Model):
                             SUM(al.unit_amount) AS total_tms,
                             0 AS remaining_hours,
                             SUM(al.unit_amount) AS total_hours,
-                            SUM(al.unit_amount) AS extra_hours
+                            SUM(al.unit_amount) AS deviation
                         FROM
                             project_project p
                             -- Link with the analytic account
