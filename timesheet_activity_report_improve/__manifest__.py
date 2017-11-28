e367ff60f3755a7d9c8f9d49b6d2a4cda43cdea3
xlm609 <xlm609@163.com>
2017-11-28 14:19:35 +0800

    [ADD][BR1187]timesheet_activity_report_improve

---
+++ timesheet_activity_report_improve/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2017 Elico corp (https://www.elico-corp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Timesheet Activities Report',
+    'version': '10.0.1.1.0',
+    'category': 'Human Resources',
+    'depends': [
+        'timesheet_activity_report',
+    ],
+    'author': 'Elico Corp',
+    'support': 'support@elico-corp.com',
+    'license': 'AGPL-3',
+    'website': 'https://www.elico-corp.com',
+    'data': [
+        'views/timesheet_report_improve.xml',
+        'views/button_view.xml',
+    ],
+    'installable': True,
+    'application': True
+}
