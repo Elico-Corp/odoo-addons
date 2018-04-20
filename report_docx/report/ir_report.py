e5ace6b438d0f49eaca4ceb44bf256481f27973b
Kevin4577 <wei_4457@qq.com>
2018-04-20 14:54:15 +0800

    [ADD]add the updated v10 report_docx

---
+++ report_docx/report/ir_report.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2018 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class IrActionReportDocx(models.Model):
+    _inherit = 'ir.actions.report.xml'
+
+    @api.model
+    def _check_selection_field_value(self, field, value):
+        if field == 'report_type' and value == 'docx':
+            return
+
+        return super(IrActionReportDocx, self)._check_selection_field_value(
+            field, value)
