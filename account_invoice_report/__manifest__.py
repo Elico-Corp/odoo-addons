460d316971efbad1607c97e0ebf25c1a9856987a
barry <barry.mu@elico-corp.com>
2018-05-03 16:54:37 +0800

    modify manifest

--- account_invoice_report/__manifest__.py
+++ account_invoice_report/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Invoice Report',
-    'version': '10.0.1.0.1',
+    'version': '10.0.1.1.0',
     'category': '',
     'depends': ['stock', 'sale'],
     'author': 'Elico Corp',
