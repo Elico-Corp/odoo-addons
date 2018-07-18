3da6cefde1829c9000feefa77448f7f1a025d515
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2018-07-18 16:09:32 +0530

    [IMP]Migrated Auth Cas Module

--- auth_cas/controllers/main.py
+++ auth_cas/controllers/main.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 # © 2016 Roméo Guillot Roméo Guillot (http://www.opensource-elanz.fr).
-# © 2016 Elico Corp (https://www.elico-corp.com).
+# © 2018 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 import urllib
-import odoo
-
-from odoo.modules.registry import RegistryManager
 from odoo.addons.web.controllers.main import login_and_redirect
-from odoo import SUPERUSER_ID
 from openid.cryptutil import randomString
-from urlparse import urlparse
+from odoo.exceptions import AccessDenied
+import urlparse
 
 from ..pycas import login
 
@@ -20,6 +17,7 @@ from odoo.http import request
 
 import werkzeug
 import logging
+
 _logger = logging.getLogger(__name__)
 
 
@@ -27,11 +25,8 @@ class Controller(http.Controller):
     _cp_path = '/auth_cas'
 
     @staticmethod
-    def get_config_static(req, dbname):
+    def get_config_static():
         """ Retrieves the module config for the CAS authentication. """
-        # registry = RegistryManager.get(dbname)
-        # with registry.cursor() as cr:
-        #     print ">>>>>>>>>>>>>>>>",cr,request
         icp = request.env['ir.config_parameter'].sudo()
         config = {
             'login_cas': icp.get_param('cas_auth.cas_activated'),
@@ -43,10 +38,8 @@ class Controller(http.Controller):
         return config
 
     @http.route('/auth_cas/get_config', type='json')
-    def get_config(self, req, dbname):
+    def get_config(self):
         """ Retrieves the module config for the CAS authentication. """
-        # registry = RegistryManager.get(dbname)
-        # with registry.cursor() as cr:
         icp = request.env['ir.config_parameter'].sudo()
         config = {
             'login_cas': icp.get_param('cas_auth.cas_activated'),
@@ -72,86 +65,47 @@ class Controller(http.Controller):
             # todo if one ticket in redirect
             # and one ticket in normal path, deal ticket
             return res
+
         return qs(url).get('ticket', '')
 
-    def _cas_login(self, redirect=None, **kw):
-        cas_url, service_url, dbname = self._get_config_url()
+    @http.route('/auth_cas/cas_authenticate', type='http', auth="none")
+    def cas_authenticate(self):
+        config = Controller.get_config_static()
+        service_url = request.httprequest.url_root
+        cas_login = config.get('host')+ ':' + config.get('port') + '/cas/login?service=' + service_url
+        # response
+        return werkzeug.utils.redirect(cas_login)
 
-        ticket = self._get_cas_ticket(request)
-        if not ticket:
-            cas_login = cas_url + '/login?service=' + service_url
-            # response
-            return werkzeug.utils.redirect(cas_login)
-            # return http.redirect_with_hash(cas_redirect)
-        else:
-            # userName = self.validateCASTicket(ticket, cas_url, service_url)
-            status, userName, cookie = login(cas_url, service_url, ticket)
-            ids = []
-            if userName and status == 0:
-                # def getUidFromUserName(userName):
-                registry = RegistryManager.get(dbname)
-                cr = registry.cursor()
-                users = registry.get('res.users')
-                ids = users.search(
-                    cr, SUPERUSER_ID, [('login', '=', userName)])
-                assert len(ids) == 1
-                cas_key = randomString(
-                    16, '0123456789abcdefghijklmnopqrstuvwxyz')
-                users.write(cr, SUPERUSER_ID, ids, {'cas_key': cas_key})
-                # set cookie for relogin
-                res = login_and_redirect(dbname, userName, cas_key)
-                return res
-        return
-
-    @http.route('/web/login', type='json', auth="none")
-    def web_login(self, redirect=None, **kw):
-
-        config = Controller.get_config_static(request, request.session.db)
-        if config.get('login_cas', False) == u'True':
-            res = self._cas_login(redirect)
-            if res:
-                return res
-
-        return "Failed"
-
-    @http.route('/auth_cas/cas_authenticate', type='http')
-    def cas_authenticate(
-            self, req, dbname, cur_url, cas_host, cas_port,
+    @staticmethod
+    def cas_authenticate_user(req, dbname, cur_url, cas_host, cas_port,
             auto_create, ticket):
         """
         Checks if the user attempts to authenticate is authorized
         to do it and, if it is, authenticate him.
         """
-
         if cas_port == -1:
             cas_server = cas_host
         else:
-            url_server = urlparse(cas_host)
+            url_server = urlparse.urlparse(cas_host)
             cas_server = url_server.scheme + '://' + url_server.netloc + ':' \
-                + str(cas_port) + url_server.path
+                         + str(cas_port) + url_server.path
         service_url = urllib.quote(cur_url, safe='')
         # The login function, from pycas, check if the ticket given
         # by CAS is a real ticket. The login of the user
         # connected by CAS is returned.
         status, idUser, cookie = login(cas_server, service_url, ticket)
         result = False
-
         if idUser and status == 0:
-            registry = RegistryManager.get(dbname)
-            cr = registry.cursor()
-            users = registry.get('res.users')
-            ids = users.search(cr, SUPERUSER_ID, [('login', '=', idUser)])
-
-            assert len(ids) < 2
+            users = request.env['res.users'].sudo()
+            # users = registry.get('res.users')
+            user_id = users.search([('login', '=', idUser)])
+            assert len(user_id) < 2
 
             # We check if the user authenticated have an odoo account
             # or if the auto_create field is True
-            if ids or auto_create == 'True':
-                if ids:
-                    user_id = ids[0]
-                # If the user have no account, we create one
-                else:
-                    user_id = users.create(cr, SUPERUSER_ID, {
+            if user_id or auto_create == 'True':
+                if not user_id:
+                    user_id = users.create({
                         'name': idUser.capitalize(), 'login': idUser})
 
                 # A random key is generated in order to verify if the
@@ -159,18 +113,19 @@ class Controller(http.Controller):
                 # try to authenticate by any other way
                 cas_key = randomString(
                     16, '0123456789abcdefghijklmnopqrstuvwxyz')
-
-                users.write(cr, SUPERUSER_ID, [user_id], {'cas_key': cas_key})
-
-                login_and_redirect(dbname, idUser, cas_key)
-
-                result = {'status': status, 'session_id': req.session_id}
+                user_id.write({'cas_key': cas_key,'password': cas_key})
+                request.cr.commit()
+                try:
+                    login_and_redirect(dbname, idUser, cas_key)
+                except AccessDenied:
+                    values = {}
+                    values['databases'] = http.db_list()
+                    values['error'] = "Wrong login/password"
+                    return request.render('web.login', values)
+                result = {'status': status}
             else:
                 result = {
-                    'status': status, 'fail': True,
-                    'session_id': req.session_id}
-
-            cr.close()
+                    'status': status, 'fail': True}
 
         if not result:
             result = {'status': status}
@@ -179,6 +134,18 @@ class Controller(http.Controller):
 
 
 class Home(main.Home):
+
+    @http.route()
+    def index(self, s_action=None, db=None, **kw):
+        ticket = self._get_cas_ticket(request)
+        if ticket:
+            config = Controller.get_config_static()
+            
+            cas_url, service_url, dbname = self._get_config_url()
+            Controller.cas_authenticate_user(
+                request, dbname, service_url, config.get('host'),
+                config.get('port'), config.get('auto_create'), ticket)
+        return super(Home, self).index(s_action, db, **kw)
 
     def _get_cas_ticket(self, request):
         # get ticket from url
@@ -195,90 +162,25 @@ class Home(main.Home):
             # todo if one ticket in redirect and one ticket
             # in normal path, deal ticket
             return res
+
         return qs(url).get('ticket', '')
 
     @staticmethod
     def _get_config_url():
-        config = Controller.get_config_static(request, request.session.db)
+        config = Controller.get_config_static()
         cas_url = config['host'] + ':' + config['port']
         service_url = request.httprequest.url_root
         dbname = request.session.db
         return cas_url, service_url, dbname
-
-    def _cas_login(self, redirect=None, **kw):
-        cas_url, service_url, dbname = self._get_config_url()
-
-        ticket = self._get_cas_ticket(request)
-        if not ticket:
-            cas_login = cas_url + '/login?service=' + service_url
-            # response
-            return werkzeug.utils.redirect(cas_login)
-            # return http.redirect_with_hash(cas_redirect)
-        else:
-            # userName = self.validateCASTicket(ticket, cas_url, service_url)
-            status, userName, cookie = login(cas_url, service_url, ticket)
-            ids = []
-            if userName and status == 0:
-                # def getUidFromUserName(userName):
-                registry = RegistryManager.get(dbname)
-                cr = registry.cursor()
-                users = registry.get('res.users')
-                ids = users.search(
-                    cr, SUPERUSER_ID, [('login', '=', userName)])
-                assert len(ids) == 1
-                cas_key = randomString(
-                    16, '0123456789abcdefghijklmnopqrstuvwxyz')
-                users.write(cr, SUPERUSER_ID, ids, {'cas_key': cas_key})
-                # set cookie for relogin
-                res = login_and_redirect(dbname, userName, cas_key)
-                return res
-        return
-
-    @http.route('/web/login', type='http', auth="none")
-    def web_login(self, redirect=None, **kw):
-        main.ensure_db()
-        config = Controller.get_config_static(request, request.session.db)
-        if config.get('login_cas', False) == u'True':
-            res = self._cas_login(redirect)
-            if res:
-                return res
-
-        if request.httprequest.method == 'GET' and redirect and \
-           request.session.uid:
-            return http.redirect_with_hash(redirect)
-
-        if not request.uid:
-            request.uid = odoo.SUPERUSER_ID
-
-        values = request.params.copy()
-        if not redirect:
-            redirect = '/web?' + request.httprequest.query_string
-        values['redirect'] = redirect
-
-        try:
-            values['databases'] = http.db_list()
-        except odoo.exceptions.AccessDenied:
-            values['databases'] = None
-
-        if request.httprequest.method == 'POST':
-            old_uid = request.uid
-            uid = request.session.authenticate(
-                request.session.db, request.params['login'],
-                request.params['password'])
-            if uid is not False:
-                return http.redirect_with_hash(redirect)
-            request.uid = old_uid
-            values['error'] = "Wrong login/password"
-        return request.render('web.login', values)
 
 
 class Session(main.Session):
 
     def _cas_logout(self):
         _logger.debug("!! cas logout")
-        # self.clear_cookie("UserName")
         casUrl, serviceUrl, dbName = Home._get_config_url()
-        logout_url = casUrl + '/logout'
+
+        logout_url = casUrl + '/cas/logout?service=' + serviceUrl
         return werkzeug.utils.redirect(logout_url)
 
     def cas_logout(self):
@@ -287,7 +189,7 @@ class Session(main.Session):
     @http.route('/web/session/logout', type='http', auth="none")
     def logout(self, redirect='/web'):
         request.session.logout(keep_db=True)
-        config = Controller.get_config_static(request, request.session.db)
+        config = Controller.get_config_static()
         if config.get('login_cas', False) == u'True':
             return self.cas_logout()
         else:
--- auth_cas/wizard/res_config.py
+++ auth_cas/wizard/res_config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2016 Roméo Guillot Roméo Guillot (http://www.opensource-elanz.fr).
-# © 2016 Elico Corp (https://www.elico-corp.com).
+# © 2018 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 import logging
@@ -9,11 +9,10 @@ from odoo import api, fields, models
 from odoo.tools.safe_eval import safe_eval
 from odoo.tools.translate import _
 from urlparse import urlparse
-from pycas import login
 
 _logger = logging.getLogger(__name__)
 try:
-    from auth_cas.pycas import login
+    from ..pycas import login
 except (ImportError, IOError) as err:
     _logger.debug(err)
 
@@ -31,14 +30,14 @@ class CasBaseConfigSettings(models.TransientModel):
     cas_activated = fields.Boolean(
         'CAS authentication activated',
         help='The CAS authentication only works if you are in a single database mode. \
-You can launch the Odoo Server with the option --db-filter=YOUR_DATABASE \
-to do so.')
+        You can launch the Odoo Server with the option --db-filter=YOUR_DATABASE \
+        to do so.')
     cas_server = fields.Char('CAS Server address', size=64)
     cas_server_port = fields.Integer('CAS Server port')
     cas_create_user = fields.Boolean(
         'Users created on the fly',
         help='Automatically create local user accounts for new users authenticating \
-via CAS')
+        via CAS', default=True)
 
     # Getter is required for fields stored in base.config.settings
     @api.model
@@ -68,47 +67,47 @@ via CAS')
             # we can activate CAS authentication and save all values
             if self.cas_server and self.cas_server_port:
                 if self.cas_activated:
-                    icp.set_param('auth_cas.cas_activated',
+                    icp.set_param('cas_auth.cas_activated',
                                   str(self.cas_activated))
                     # There is no error
                     error = False
-                icp.set_param('auth_cas.cas_server', self.cas_server)
-                icp.set_param('auth_cas.cas_server_port',
+                icp.set_param('cas_auth.cas_server', self.cas_server)
+                icp.set_param('cas_auth.cas_server_port',
                               self.cas_server_port)
             # Else, there is one error
             # If the host field is valid, we save it and the default port value
             elif self.cas_server:
-                icp.set_param('auth_cas.cas_server', self.cas_server)
-                icp.set_param('auth_cas.cas_server_port', default_port)
+                icp.set_param('cas_auth.cas_server', self.cas_server)
+                icp.set_param('cas_auth.cas_server_port', default_port)
             # Else, the host field is empty,
             # but not the port field: we save it and the default host value
             else:
-                icp.set_param('auth_cas.cas_server_port',
+                icp.set_param('cas_auth.cas_server_port',
                               self.cas_server_port)
-                icp.set_param('auth_cas.cas_server', default_host)
+                icp.set_param('cas_auth.cas_server', default_host)
         # If error is True, there is at least one error,
         # so we deactivate CAS authentication
         if error:
-            icp.set_param('auth_cas.cas_activated', 'False')
+            icp.set_param('cas_auth.cas_activated', 'False')
             # If the host field is empty, we save his default value
             if not self.cas_server:
-                icp.set_param('auth_cas.cas_server', default_host)
+                icp.set_param('cas_auth.cas_server', default_host)
             # Else, it seems it is the port field
             # which is empty: we save his default value
             else:
-                icp.set_param('auth_cas.cas_server_port', default_port)
+                icp.set_param('cas_auth.cas_server_port', default_port)
 
         # We save the field used to know
         # if users have to be created on the fly or not
-        icp.set_param('auth_cas.cas_create_user', str(self.cas_create_user))
+        icp.set_param('cas_auth.cas_create_user', str(self.cas_create_user))
 
     @api.multi
     def check_cas_server(self):
         self.ensure_one()
         """Check if CAS paramaters (host and port) are valids"""
         title = 'cas_check_fail'
-        message = 'Parameters are incorrect\nThere seems to be an \
-error in the configuration.'
+        message = 'Parameters are incorrect\nThere seems to be an ' \
+                  'error in the configuration.'
         if self.cas_server_port == -1:
             cas_server = self.cas_server
         else:
@@ -120,14 +119,13 @@ error in the configuration.'
             # a CAS host and if parameters given are bad
             # (Parameters are empty, except the adress of the CAS server,
             # so there are always bad !)
-            res = login(cas_server, 'casuser', 'Mellon')
-            if res[0] == 5:
+            res = login(cas_server, ' ', ' ')
+            if res[0] == 3:
                 title = 'cas_check_success'
-                message = 'Parameters are correct\nThe CAS server is well \
-configured !'
+                message = 'Parameters are correct\nThe CAS server is well ' \
+                          'configured !'
         except:
             pass
-
         # At the moment, I only found this method
         # in order to show a message after a request
         raise ValidationError(_(title) + '\n' + _(message))
