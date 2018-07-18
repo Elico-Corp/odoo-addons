b8897cf0c09abec0c07696c7ec1a35e51103dd7f
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2018-07-18 17:06:47 +0530

    [IMP]Improved Pylint 3

--- auth_cas/wizard/__init__.py
+++ auth_cas/wizard/__init__.py
@@ -1,6 +1,3 @@
 # -*- coding: utf-8 -*-
-# © 2016 Roméo Guillot Roméo Guillot (http://www.opensource-elanz.fr).
-# © 2018 Elico Corp (https://www.elico-corp.com).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from . import res_config
