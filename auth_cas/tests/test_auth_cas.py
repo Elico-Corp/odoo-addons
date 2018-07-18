9c91da77cd534a22b7e17d166057bfd6e9e48d16
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2018-07-18 18:22:41 +0530

    [IMP]Migrated Auth Cas Module

--- auth_cas/tests/test_auth_cas.py
+++ auth_cas/tests/test_auth_cas.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2018 Elico Corp (https://www.elico-corp.com).
+# © 2016-2018 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo.tests import common
 from mock import patch
-from odoo.addons.auth_cas.controllers.main import Controller
+from ..controllers.main import Controller
 
 
 class TestAuthCas(common.TransactionCase):
