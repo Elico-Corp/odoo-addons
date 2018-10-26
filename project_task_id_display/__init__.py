eda99d36cd974410096fc62c1b58812929a630f9
Shall Sheng <shall.sheng@elico-corp.com>
2018-10-26 16:25:46 +0800

    some small details

--- project_task_id_display/__init__.py
+++ project_task_id_display/__init__.py
@@ -1,3 +1,1 @@
 # -*- coding: utf-8 -*-
-# © 2015-18 Elico corp (https://www.elico-corp.com)
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
--- project_task_id_display/__manifest__.py
+++ project_task_id_display/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2015-18 Elico corp (https://www.elico-corp.com)
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Project Task ID Display',
     'version': '10.0.1.0.0',
