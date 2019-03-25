7f01bec3ca0fd68b05b9fff141aab3f11ddafbf2
Reinhard <779081643@qq.com>
2019-03-25 16:50:49 +0800

    [IMP] imp code

--- project_project_category/models/__init__.py
+++ project_project_category/models/__init__.py
@@ -1,4 +1,1 @@
-# © 2016-2019 Elico Corp (https://www.elico-corp.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 from . import project_project_category
--- project_project_category/models/project_project_category.py
+++ project_project_category/models/project_project_category.py
@@ -1,13 +1,13 @@
 # © 2016-2019 Elico Corp (https://www.elico-corp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class ProjectProjectCategory(models.Model):
     _name = 'project.project.category'
 
-    name = fields.Char('Name', required=True, translate=True, select=True)
+    name = fields.Char('Name', required=True, translate=True, index=True)
     description = fields.Char('Description', translate=True)
     active = fields.Boolean('Active', default=True)
 
