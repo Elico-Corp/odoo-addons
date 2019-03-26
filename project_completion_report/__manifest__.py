cf032d9bc5419d2d96246cf355d60b0b55997455
Reinhard <779081643@qq.com>
2019-03-26 14:32:09 +0800

    [FIX] fix depends

--- project_completion_report/__manifest__.py
+++ project_completion_report/__manifest__.py
@@ -10,10 +10,10 @@
     "author": "Elico Corp",
     "license": "LGPL-3",
     "application": False,
-    "installable": False,
+    "installable": True,
     "depends": [
-        "project_timesheet",
-        "project_issue_sheet",
+        "hr_timesheet",
+        "helpdesk_timesheet",
         "project_project_category",
         "business_requirement_deliverable_project",
     ],
