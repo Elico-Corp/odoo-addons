fa94e3fd9a0faf515512eab406701f449d70922a
Reinhard <779081643@qq.com>
2019-03-26 14:26:32 +0800

    [FIX] fix copyright

--- project_completion_report/__manifest__.py
+++ project_completion_report/__manifest__.py
@@ -1,5 +1,5 @@
 # © 2016-2019 Elico Corp (https://www.elico-corp.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Project Completion Report",
     "summary": "Follow-up project completion (estimated / consumed)",
@@ -8,7 +8,7 @@
     'website': 'https://www.elico-corp.com',
     'support': 'support@elico-corp.com',
     "author": "Elico Corp",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "application": False,
     "installable": False,
     "depends": [
--- project_completion_report/report/project_completion_report.py
+++ project_completion_report/report/project_completion_report.py
@@ -1,5 +1,5 @@
 # © 2016-2019 Elico Corp (https://www.elico-corp.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo import models, fields, tools
 
