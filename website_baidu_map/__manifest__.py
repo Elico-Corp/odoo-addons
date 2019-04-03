23e5ed364ff7fc50f82ac73658954ee84ad59331
Ruter Lv <ruter.lv@elico-corp.com>
2019-04-03 16:55:26 +0800

    [MIG] Migrate website_baidu_map from 8.0 to 12.0

---
+++ website_baidu_map/__manifest__.py
@@ -0,0 +1,17 @@
+# 2016-2019 Elico Corp (https://www.elico-corp.com).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Website Baidu Map',
+    'version': '12.0.1.0.0',
+    'category': 'map',
+    'depends': ['base', 'website'],
+    'author': 'Elico Corp',
+    'license': 'LGPL-3',
+    'website': 'https://www.elico-corp.com',
+    'data': [
+        'views/templates.xml',
+        'views/snippets.xml'
+    ],
+    'installable': True,
+    'application': False
+}
