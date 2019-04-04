48a120751513dd56e3578c08df81d2985b11f3d1
Ruter Lv <ruter.lv@elico-corp.com>
2019-04-04 09:13:51 +0800

    [MIG] Migrate website_baidu_map_multi from 8.0 to 12.0

--- website_baidu_map_multi/__manifest__.py
+++ website_baidu_map_multi/__manifest__.py
@@ -1,18 +1,17 @@
-# -*- coding: utf-8 -*-
-#   2016 Elico Corp (https://www.elico-corp.com).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# 2016-2019 Elico Corp (https://www.elico-corp.com).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Website Baidu Map Multi',
-    'version': '8.0.1.0.1',
+    'version': '12.0.1.0.0',
     'category': 'Hidden',
     'depends': ['website', 'base', 'website_baidu_map'],
     'author': 'Elico-Corp,Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'website': 'https://www.elico-corp.com',
-    'images': [],
     'data': [
         'views/company_shop_view.xml',
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/templates.xml',
     ],
     'installable': True,
     'application': False
