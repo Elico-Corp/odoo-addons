ian-knight-uofa/odoo-cas

Flake8+pylint pass 2

res_config.py
@@ -19,7 +19,7 @@ default_host = 'https://localhost'
 default_port = 8443
 
 
-class cas_base_config_settings(osv.TransientModel):
+class CasBaseConfigSettings(osv.TransientModel):
     """
     The fields declared here are used to manage settings of the CAS server.
     """

Elico-Corp/odoo-addons

website_captcha_nogoogle/models/res_config.py
@@ -5,7 +5,7 @@
 from openerp import fields, models
 
 
-class website_config_settings(models.TransientModel):
+class WebsiteConfigSettings(models.TransientModel):
     _inherit = 'website.config.settings'
 
     captcha_crypt_password = fields.Char(