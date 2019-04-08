694c6512429673ced331db4d8caba62edf0611b7
Ruter Lv <ruter.lv@elico-corp.com>
2019-04-08 18:00:57 +0800

    [MIG] Migrate website_event_baidu_map from 8.0 to 12.0

--- website_event_baidu_map/__manifest__.py
+++ website_event_baidu_map/__manifest__.py
@@ -1,20 +1,18 @@
-# -*- coding: utf-8 -*-
-# © 2015 Elico corp (www.elico-corp.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2015-2019 Elico corp (www.elico-corp.com)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Website Event Baidu Map',
-    'version': '8.0.1.0.2',
+    'version': '12.0.1.0.0',
     'category': 'Website',
     'depends': [
-        'website',
-        'base',
+        'website_baidu_map',
         'website_event',
     ],
     'author': 'Elico Corp',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'website': 'https://www.elico-corp.com',
     'data': [
-        'website_event_baidu_map.xml',
+        'views/website_event_baidu_map.xml',
     ],
     'installable': True,
     'application': False,
