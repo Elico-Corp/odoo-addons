1a07609406fcb39d12cb93e5951ac03a92033d42
Ruter Lv <ruter.lv@elico-corp.com>
2019-04-08 14:46:08 +0800

    fix captcha validation logic

--- website_captcha_nogoogle_crm/controllers/main.py
+++ website_captcha_nogoogle_crm/controllers/main.py
@@ -1,22 +1,33 @@
 # © 2015-2018 Elico corp (www.elico-corp.com)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
+import json
+
 from odoo import http
 from odoo.http import request
-import odoo.addons.website_crm.controllers.main as main
+from odoo.addons.website_form.controllers.main import WebsiteForm
 
 
-class ContactUs(main.contactus):
+class WebsiteForm(WebsiteForm):
 
-    @http.route(['/crm/contactus'], type='http', auth="public",
-                website=True, multilang=True)
-    def contactus(self, *args, **kw):
-        crypt_challenge = kw.pop('captcha_challenge_field', None)
-        response = kw.pop('captcha_response_field', None)
-        if not kw or not crypt_challenge or not response:
-            pass
-        elif request.website.is_captcha_valid(crypt_challenge, response):
-            return super(ContactUs, self).contactus(*args, **kw)
-        values = dict(kw, error=['captcha_response_field'], kwargs=kw.items())
-        return request.website.render(
-            kw.get("view_from", "website.contactus"), values)
+    @http.route('/website_form/<string:model_name>', type='http',
+                auth="public", methods=['POST'], website=True)
+    def website_form(self, model_name, **kwargs):
+        res = super(WebsiteForm, self).website_form(model_name, **kwargs)
+        data = json.loads(res.data.decode('utf-8'))
+        if model_name == 'crm.lead' and \
+                not request.params.get('state_id') and \
+                data is not False:
+            crypt_challenge = kwargs.get('captcha_challenge_field', None)
+            response = kwargs.get('captcha_response_field', None)
+            if not crypt_challenge or not response:
+                pass
+            elif request.website.is_captcha_valid(crypt_challenge, response):
+                return res
+            data = {
+                'error_fields': data['error_fields'] +
+                                    ['captcha_response_field']
+                if 'error_fields' in data else ['captcha_response_field']
+            }
+            res = json.dumps(data)
+        return res
