a7c99dc6241d4d9607ea1ae5e1987ba71d5a6623
Ruter Lv <ruter.lv@elico-corp.com>
2019-04-08 15:55:11 +0800

    [MIG] Migrate website_header_language_switcher from 8.0 to 12.0

--- website_header_language_switcher/__manifest__.py
+++ website_header_language_switcher/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
-# © 2015 Elico corp (www.elico-corp.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2015-2019 Elico corp (www.elico-corp.com)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Website Header Language Switch',
-    'version': '8.0.0.0.2',
+    'version': '12.0.1.0.0',
     'category': 'website',
     'depends': ['base', 'website'],
     'author': 'Elico Corp,Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'website': 'https://www.elico-corp.com',
     'images': [],
-    'data': ['header_language.xml'],
+    'data': ['views/header_language.xml'],
     'installable': True,
     'application': False,
 }
