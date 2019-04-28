8a210d832727813aed3613fa0901908e16912b18
Ruter Lyu <ruter.lv@elico-corp.com>
2019-04-28 11:32:18 +0800

    fix syntax

--- website_event_snippet_image/__manifest__.py
+++ website_event_snippet_image/__manifest__.py
@@ -8,7 +8,7 @@
         'website_event',
     ],
     'author': 'Elico Corp',
-    'support': 'Elico Corp'
+    'support': 'Elico Corp',
     'license': 'LGPL-3',
     'website': 'https://www.elico-corp.com',
     'data': [
