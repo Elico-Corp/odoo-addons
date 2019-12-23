105459ed93fba96c036d2a6c1b39552f115d51e7
Benjamin <benjamin@le-filament.com>
2019-12-23 10:58:16 +0100

    [MIG] Make module installable in v12

---
+++ auth_cas/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 Roméo Guillot Roméo Guillot (http://www.opensource-elanz.fr).
+# © 2016-2018 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'CAS Authentication',
+    'version': '12.0.1.0.0',
+    'category': 'Authentication',
+    'summary': 'CAS Authentication',
+    'author': 'Elico Corp',
+    'license': 'AGPL-3',
+    'support': 'support@elico-corp.com',
+    'website': 'https://www.elico-corp.com/',
+    'depends': [
+        'base_setup',
+        'web'
+    ],
+    'data': [
+        'wizard/res_config_view.xml',
+        'views/auth_cas_view.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ auth_cas/models/res_users.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# © 2016 Roméo Guillot Roméo Guillot (http://www.opensource-elanz.fr).
+# © 2016-2018 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+
+from odoo import api, fields, models
+from odoo.exceptions import AccessDenied
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class ResUsers(models.Model):
+    """The field declared here is used
+    in order to check autenticity of logins
+    """
+
+    _inherit = 'res.users'
+
+    cas_key = fields.Char('CAS Key', size=16, readonly=True)
+
+    @api.model
+    def check_credentials(self, password):
+        """Check Authenticy of logins"""
+        # We try to connect the user with his password by the standard way
+        try:
+            return super(ResUsers, self).check_credentials(password)
+        # If it failed, we try to do it thanks to
+        # the cas key created by the Controller
+        except AccessDenied:
+            if not password:
+                raise AccessDenied()
+            self._cr.execute("""SELECT COUNT(1)
+                                FROM res_users
+                               WHERE id=%s
+                                 AND cas_key=%s""",
+                             (int(self._uid), password))
+            if not self._cr.fetchone()[0]:
+                raise AccessDenied()
