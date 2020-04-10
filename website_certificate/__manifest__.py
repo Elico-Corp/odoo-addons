954f0fcbf90a8ab332e12603afc1290570a9fdaa
CaiBen <ben.cai@elico-corp.com>
2020-04-10 09:55:47 +0800

    [12.0][MIG]Mirgrate website_certificate

---
+++ website_certificate/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2020 Elico Corp (www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Website Certificate',
+    'version': '12.0.1.0.0',
+    'category': 'website',
+    'support': 'support@elico-corp.com',
+    'depends': [
+        'website',
+    ],
+    'author': 'Elico Corp',
+    'license': 'AGPL-3',
+    'website': 'https://www.elico-corp.com',
+    'data': [
+        'certificate_view.xml',
+    ],
+    'installable': True,
+    'application': False
+}
