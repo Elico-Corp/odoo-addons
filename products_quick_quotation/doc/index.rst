352b45737c9b9a07c9736952d28c5369e472e526
wanghaoran <whr428@163.com>
2024-03-05 23:29:32 +0800

    initial branch

--- products_quick_quotation/doc/index.rst
+++
@@ -1,7 +0,0 @@
-Create Quick Quotation from Products
-====================================
-- This module will helps you to Create Quotation from Products.
-
-Installation
-============
-- Install the module normally like other modules.
--- products_quick_quotation/models/models.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models
-
-class View(models.Model):
-    _inherit = 'ir.ui.view'
-    
-    type = fields.Selection(selection_add=[
-        ('quickquotationview', 'Qucik Quotation View')
-    ], ondelete={'quickquotationview': 'cascade'})
